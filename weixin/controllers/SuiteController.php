f821182275751c715225cb01bfc026ca2c3c6bef
wangzhen <wangzhen@xywy.com>
2018-04-24 14:20:29 +0800

    js

--- weixin/controllers/SuiteController.php
+++ weixin/controllers/SuiteController.php
@@ -39,6 +39,7 @@ class SuiteController extends Controller
                 $xml = $this->mpWechat->parseRequestXml($postStr, $_GET['msg_signature'], $_GET['timestamp'], $nonce = $_GET['nonce'], $_GET['encrypt_type']);
                 $openid = $xml['FromUserName'];
                 $doctor_id = str_replace('qrscene_', '', $xml['EventKey']);
+                return self::sendText($openid, $xml['ToUserName'], json_encode($xml));
 
                 //扫码记录
                 $weOpenid=WeOpenid::findOne(['openid'=>$openid,'doctorid'=>$doctor_id]);
