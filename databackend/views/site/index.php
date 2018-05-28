0ac55a7ff2deed279201e1ce5f81029e2ef0d5f7
xiaohaoyong <xiaohaoyong@126.com>
2018-05-28 11:15:57 +0800

    数据导出

--- databackend/views/site/index.php
+++ databackend/views/site/index.php
@@ -171,7 +171,7 @@ databackend\assets\IndexAsset::register($this);
                             <th>今日宣教</th>
                             <th>已宣教数</th>
                             <th>管理服务率</th>
-                            <th>规范化</th>
+                            <th>数据</th>
                         </tr>
                     </thead>
                     <tbody>
