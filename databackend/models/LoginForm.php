c4a6488231294ea8333b0f20214baa103bc7b7d1
xiaohaoyong <xiaohaoyong@126.com>
2018-05-29 18:33:15 +0800

    ico

--- databackend/models/LoginForm.php
+++ databackend/models/LoginForm.php
@@ -10,9 +10,17 @@ namespace databackend\models;
 
 
 use common\models\DataUser;
+use yii\base\Model;
 
-class LoginForm extends \common\models\LoginForm
+class LoginForm extends Model
 {
+    public $username;
+    public $password;
+    public $rememberMe = true;
+
+    private $_user;
+
+
     /**
      * @inheritdoc
      */
@@ -33,10 +41,65 @@ class LoginForm extends \common\models\LoginForm
     public function validateUsername($attribute, $params)
     {
         $dataUser=DataUser::findOne(['username',$this->username]);
-        if($dataUser && $dataUser->type!=1)
+        if($dataUser->type!=1)
         {
             $this->addError($attribute, '医院账号无法登陆监管后台，请访问hospital.child登陆');
         }
     }
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'username' => '用户名',
+            'password' => '密码',
+            'rememberMe' => '记住我'
+        ];
+    }
+    /**
+     * Validates the password.
+     * This method serves as the inline validation for password.
+     *
+     * @param string $attribute the attribute currently being validated
+     * @param array $params the additional name-value pairs given in the rule
+     */
+    public function validatePassword($attribute, $params)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->getUser();
+            if (!$user || !$user->validatePassword($this->password)) {
+                $this->addError($attribute, 'Incorrect username or password.');
+            }
+        }
+    }
 
+    /**
+     * Logs in a user using the provided username and password.
+     *
+     * @return bool whether the user is logged in successfully
+     */
+    public function login()
+    {
+        if ($this->validate()) {
+
+            return \Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Finds user by [[username]]
+     *
+     * @return User|null
+     */
+    protected function getUser()
+    {
+        if ($this->_user === null) {
+            $this->_user = \databackend\models\User::findByUsername($this->username);
+        }
+
+        return $this->_user;
+    }
 }
