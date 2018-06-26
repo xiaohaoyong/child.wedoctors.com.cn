8baa69121ab43bece4fe9f16128aa75149fd166c
xiaohaoyong <xiaohaoyong@126.com>
2018-06-26 18:39:23 +0800

    体检提醒文案修改

--- backend/views/child-info/index.php
+++ backend/views/child-info/index.php
@@ -13,117 +13,120 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="child-info-index">
     <?php echo $this->render('_search', ['model' => $searchModel]); ?>
     <hr>
-    <?= GridView::widget([
-        'options'=>['class' => 'col-sm-12','style'=>"font-size: 12px;"],
-        'dataProvider' => $dataProvider,
-        'columns' => [
+    <div class="row">
+        <?= GridView::widget([
+            'options'=>['class' => 'col-sm-12','style'=>"font-size: 12px;"],
+            'dataProvider' => $dataProvider,
+            'columns' => [
 
-            'name',
-            [
+                'name',
+                [
 
-                'attribute' => '联系电话',
-                'value' => function ($e) {
-                    return \common\models\User::findOne($e->userid)->phone;
-                }
-            ],
-            [
-
-                'attribute' => '性别',
-                'value' => function ($e) {
-                    return \common\models\ChildInfo::$genderText[$e->gender];
-                }
-            ],
-            [
-                'attribute' => '年龄',
-                'value' => function ($e) {
-                    $DiffDate = \common\helpers\StringHelper::DiffDate(date('Y-m-d', time()), date('Y-m-d', $e->birthday));
-                    if($DiffDate[0]) {
-                        $age=$DiffDate[0]."岁";
-                    }elseif($DiffDate[1]){
-                        $age=$DiffDate[1]."月";
-                    }else{
-                        $age=$DiffDate[2]."天";
+                    'attribute' => '联系电话',
+                    'value' => function ($e) {
+                        return \common\models\User::findOne($e->userid)->phone;
                     }
-                    return $age;
-                }
-            ],
-            [
-                'attribute' => '父母',
-                'value' => function ($e) {
-                    return $e->parent->mother || $e->parent->father?$e->parent->mother."/".$e->parent->father:"无";
-                }
-            ],
-            [
-                'attribute' => '母亲电话',
-                'value' => function ($e) {
-                    return $e->parent->mother_phone ? $e->parent->mother_phone : "无";
-                }
-            ],
-            [
-                'attribute' => '父亲电话',
-                'value' => function ($e) {
-                    return $e->parent->father_phone ? $e->parent->father_phone : "无";
-                }
-            ],
-            [
-                'attribute' => '联系人姓名',
-                'value' => function ($e) {
-                    return $e->parent->field11 ? $e->parent->field11 : "无";
-                }
-            ],
-            [
-                'attribute' => '联系人电话',
-                'value' => function ($e) {
-                    return $e->parent->field12 ? $e->parent->field12 : "无";
-                }
-            ],
-            [
-                'attribute' => '签约社区',
-                'format'=>'raw',
-                'value' => function ($e) {
+                ],
+                [
 
-
-                    $file3=$e->sign->level==1 ? $e->doctor[0]->name : "--";
-
-                    return $file3;
-
-                }
-            ],
-
-            [
-                'attribute' => '签约时间',
-                'value' => function ($e) {
-
-                    return $e->sign->level == 1 ? date('Y-m-d H:i', $e->sign->createtime) : "无";
-                }
-            ],
-            [
-                'attribute' => '签约状态',
-                'value' => function ($e) {
-                    if($e->sign->level!=1)
-                    {
-                        $return="未签约";
-                    }else{
-                        if($e->source<=38){
-                            $return="已签约未关联";
-
-                        }else {
-                            $return = "已签约";
+                    'attribute' => '性别',
+                    'value' => function ($e) {
+                        return \common\models\ChildInfo::$genderText[$e->gender];
+                    }
+                ],
+                [
+                    'attribute' => '年龄',
+                    'value' => function ($e) {
+                        $DiffDate = \common\helpers\StringHelper::DiffDate(date('Y-m-d', time()), date('Y-m-d', $e->birthday));
+                        if($DiffDate[0]) {
+                            $age=$DiffDate[0]."岁";
+                        }elseif($DiffDate[1]){
+                            $age=$DiffDate[1]."月";
+                        }else{
+                            $age=$DiffDate[2]."天";
                         }
+                        return $age;
                     }
-                    return $return;
-                }
-            ],
-            [
-                'attribute' => '体检数据',
-                'value' => function ($e) {
-                    $count = \common\models\Examination::find()->andFilterWhere(['childid'=>$e->id])->count();
-                    return $count;
-                }
-            ],
-            [
-                'class' => 'common\components\grid\ActionColumn',
-                'template' => '
+                ],
+                [
+                    'attribute' => '父母',
+                    'value' => function ($e) {
+                        return $e->parent->mother || $e->parent->father?$e->parent->mother."/".$e->parent->father:"无";
+                    }
+                ],
+                [
+                    'attribute' => '母亲电话',
+                    'value' => function ($e) {
+                        return $e->parent->mother_phone ? $e->parent->mother_phone : "无";
+                    }
+                ],
+                [
+                    'attribute' => '父亲电话',
+                    'value' => function ($e) {
+                        return $e->parent->father_phone ? $e->parent->father_phone : "无";
+                    }
+                ],
+                [
+                    'attribute' => '联系人姓名',
+                    'value' => function ($e) {
+                        return $e->parent->field11 ? $e->parent->field11 : "无";
+                    }
+                ],
+                [
+                    'attribute' => '联系人电话',
+                    'value' => function ($e) {
+                        return $e->parent->field12 ? $e->parent->field12 : "无";
+                    }
+                ],
+                [
+                    'attribute' => '签约社区',
+                    'format'=>'raw',
+                    'value' => function ($e) {
+
+                        $sign = \common\models\DoctorParent::findOne(['parentid'=>$e->userid,'level'=>1]);
+
+                        $file3="--";
+                        if($sign->level==1){
+                            $doctorParent=\common\models\DoctorParent::findOne(['parentid'=>$e->userid,'level'=>1]);
+                            $doctor=\common\models\UserDoctor::findOne(['userid'=>$doctorParent->doctorid]);
+                            $file3=$doctor?$doctor->name:"==";
+                        }
+
+                        return $file3;
+                    }
+                ],
+
+                [
+                    'attribute' => '签约时间',
+                    'value' => function ($e) {
+
+                        $sign = \common\models\DoctorParent::findOne(['parentid'=>$e->userid,'level'=>1]);
+
+                        return $sign->level == 1 ? date('Y-m-d H:i', $sign->createtime) : "无";
+                    }
+                ],
+                [
+                    'attribute' => '签约状态',
+                    'value' => function ($e) {
+                        $sign = \common\models\DoctorParent::findOne(['parentid'=>$e->userid,'level'=>1]);
+
+                        if($sign->level!=1)
+                        {
+                            $return="未签约";
+                        }else{
+                            if($e->parent->source<=38){
+                                $return="已签约未关联";
+
+                            }else {
+                                $return = "已签约";
+                            }
+                        }
+                        return $return;
+                    }
+                ],
+                [
+                    'class' => 'common\components\grid\ActionColumn',
+                    'template' => '
                 <div class="btn-group dropup">
                     <a class="btn btn-circle btn-default btn-sm" href="javascript:;" data-toggle="dropdown" aria-expanded="false">
                         <i class="icon-settings"></i> 记录 <i class="fa fa-angle-up"></i></a>
@@ -131,18 +134,19 @@ $this->params['breadcrumbs'][] = $this->title;
                         <li>{articleuser} </li><li>{childhealthrecord}</li><li>{download}</li>
                     </ul>
                 </div>',
-                'buttons' => [
-                    'articleuser' => function ($url, $model, $key) {
-                        return Html::a('<span class="fa fa-database"></span> 宣教记录', \yii\helpers\Url::to(['article-user/index', 'ArticleUserSearchModel[childid]' => $model->id]));
-                    },
-                    'childhealthrecord' => function ($url, $model, $key) {
-                        return Html::a('<span class="fa fa-database"></span> 健康档案', \yii\helpers\Url::to(['child-info/view', 'id' => $model->id]));
-                    },
-                    'download' => function ($url, $model, $key) {
-                        return Html::a('<span class="fa fa-database"></span> 下载宣教记录', \yii\helpers\Url::to(['article-user/download', 'childid' => $model->id]));
-                    }
+                    'buttons' => [
+                        'articleuser' => function ($url, $model, $key) {
+                            return Html::a('<span class="fa fa-database"></span> 宣教记录', \yii\helpers\Url::to(['article-user/index', 'ArticleUserSearchModel[childid]' => $model->id]));
+                        },
+                        'childhealthrecord' => function ($url, $model, $key) {
+                            return Html::a('<span class="fa fa-database"></span> 健康档案', \yii\helpers\Url::to(['child-info/view', 'id' => $model->id]));
+                        },
+                        'download' => function ($url, $model, $key) {
+                            return Html::a('<span class="fa fa-database"></span> 下载宣教记录', \yii\helpers\Url::to(['article-user/download', 'childid' => $model->id]));
+                        }
+                    ],
                 ],
             ],
-        ],
-    ]); ?>
+        ]); ?>
+    </div>
 </div>
