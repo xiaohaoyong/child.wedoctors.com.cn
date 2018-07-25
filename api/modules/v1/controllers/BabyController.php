89c7741e852d2e49d8f2265523cf1f6cade67d45
xiaohaoyong <xiaohaoyong@126.com>
2018-07-25 17:34:08 +0800

    增加接口版本控制

--- api/modules/v1/controllers/BabyController.php
+++ api/modules/v1/controllers/BabyController.php
@@ -16,7 +16,7 @@ class BabyController extends \api\controllers\BabyController
 {
     public function actionCollectionList()
     {
-        $query=BabyToolLike::find()->andFilterWhere(['userid'=>$this->userid])->andFilterWhere(['type'=>3]);
+        $query=BabyToolLike::find()->andFilterWhere(['loginid'=>$this->userLogin->id])->andFilterWhere(['type'=>2]);
         $totle=$query->count();
         $pages = new Pagination(['totalCount' =>$totle, 'pageSize' => 10]);
         $datas = $query->offset($pages->offset)->limit($pages->limit)->all();
