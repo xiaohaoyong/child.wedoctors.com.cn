510663c0b42f0e70f5defbb3da26c2aa966f7242
xiaohaoyong <xiaohaoyong@126.com>
2018-08-07 11:19:52 +0800

    下载异步生成文件

--- databackend/views/child-info/_search.php
+++ databackend/views/child-info/_search.php
@@ -37,7 +37,7 @@ use yii\widgets\ActiveForm;
     <div class="form-group">
         <?= Html::submitButton('搜索', ['class' => 'btn btn-primary']) ?>
         <?= Html::resetButton('重置', ['class' => 'btn btn-default']) ?>
-        <?= Html::button('下载', ['data-toggle' => 'modal', 'data-target' => '#myModal','class' => 'btn btn-primary']) ?>
+        <?= Html::button('下载(测试)', ['data-toggle' => 'modal', 'data-target' => '#myModal','class' => 'btn btn-primary']) ?>
 
         <div class="help-block"></div>
     </div>
