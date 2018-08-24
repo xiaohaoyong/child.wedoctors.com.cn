a191129531480578b85a2cb39ac9de84788e79b9
xiaohaoyong <xiaohaoyong@126.com>
2018-08-24 17:00:22 +0800

    新认证用户推送近期宣教

--- common/components/Log.php
+++ common/components/Log.php
@@ -26,6 +26,7 @@ class Log
     public function saveLog()
     {
         $file = __LOG__ . $this->name."-".date('Y-m-d') .".log";
+        var_dump($file);exit;
         file_put_contents($file, $this->lineLog . "\n", FILE_APPEND);
     }
 
