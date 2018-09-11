dffcc53e57c7500885fa10c724bc4d4fac4309ab
xiaohaoyong <xiaohaoyong@126.com>
2018-09-11 15:36:48 +0800

    当日未完成注册用户提醒 增加统计功能

--- hospital/views/layouts/left.php
+++ hospital/views/layouts/left.php
@@ -11,19 +11,23 @@
                     ['label' => '仪表盘','icon' => 'dashboard','url' => \yii\helpers\Url::to(['site/index']),],
                     ['label' => '管辖儿童','icon' => 'archive','url' => \yii\helpers\Url::to(['child-info/index']),],
                     ['label' => '社区管理','icon' => 'hospital-o','url' => \yii\helpers\Url::to(['user-doctor/index']),],
-                    ['label' => '中医指导库','icon' => 'file-text-o','url' => \yii\helpers\Url::to(['article/zindex?ArticleSearchModel[type]=1'])],
-                    ['label' => '宣教任务','icon' => 'send','url' => \yii\helpers\Url::to(['/article-send'])],
-
-                    ['label' => '宣教知识库','icon' => 'file-text-o','url' => "#",
+                    ['label' => '中医指导库','icon' => 'file-text-o','url' => \yii\helpers\Url::to(['article/zindex?ArticleSearchModel[type]=1']),
                         'items' => [
-                            ['label' => '列表', 'options' => ['class' => 'header'],'url' => \yii\helpers\Url::to(['article/index'])],
-                            ['label' => '添加', 'options' => ['class' => 'header'],'url' => \yii\helpers\Url::to(['article/create'])],
+                            ['label' => '指导文章列表', 'url' => ['article/zindex?ArticleSearchModel[type]=1']],
+                            ['label' => '宣教任务（设置自动）', 'url' => ['/article-send']],
+                        ]
+                    ],
+                    ['label' => '健康知识库','icon' => 'file-text-o','url' => "#",
+                        'items' => [
+                            ['label' => '文章列表', 'url' => ['article/index']],
+                            ['label' => '添加文章', 'url' => ['article/create']],
                         ]
                     ],
                     ['label' => '工具','icon' => 'send','url' => "#",
                         'items' => [
-                            ['label' => '通知列表', 'options' => ['class' => 'header'],'url' => \yii\helpers\Url::to(['article/tindex?ArticleSearchModel[type]=2'])],
-                            ['label' => '发布通知', 'options' => ['class' => 'header'],'url' => \yii\helpers\Url::to(['article/tongzhi'])],
+                            ['label' => '通知列表', 'url' => ['article/tindex?ArticleSearchModel[type]=2']],
+                            ['label' => '发布通知', 'url' => ['article/tongzhi']],
+                            ['label' => '平台召回用户统计', 'url' => ['/push-log']],
                         ]
                     ],
 
