8be21dddbce1ae6bc20bbbc4fc85de5c438d596c
xiaohaoyong <xiaohaoyong@126.com>
2018-11-05 13:54:44 +0800

    预约系统数据库以及model

---
+++ hospital/models/AppointSearchModels.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace hospital\models;
+
+use common\models\ChildInfo;
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Appoint;
+
+/**
+ * AppointSearchModels represents the model behind the search form about `\common\models\Appoint`.
+ */
+class AppointSearchModels extends Appoint
+{
+    public $child_name;
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'userid', 'doctorid', 'createtime', 'appoint_time', 'appoint_date', 'type', 'childid', 'phone'], 'integer'],
+            [['child_name'],'string']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        $return = parent::attributeLabels();
+        $return ['child_name']='儿童姓名';
+        return $return;
+    }
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Appoint::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+        $doctorid=\common\models\UserDoctor::findOne(['hospitalid'=>\Yii::$app->user->identity->hospital]);
+
+        $hospitalid=Yii::$app->user->identity->hospital;
+        if($this->child_name){
+            $childids=ChildInfo::find()->select('id')->andWhere(['source'=>$hospitalid])->andWhere(['name'=>$this->child_name])->column();
+            if($childids) {
+                $query->andFilterWhere(['in', 'childid', $childids]);
+            }else{
+                $query->andFilterWhere(['in', 'childid', [0]]);
+            }
+        }
+
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'userid' => $this->userid,
+            'doctorid' => $doctorid,
+            'createtime' => $this->createtime,
+            'appoint_time' => $this->appoint_time,
+            'appoint_date' => $this->appoint_date,
+            'type' => $this->type,
+            'phone' => $this->phone,
+        ]);
+        $query->orderBy([self::primaryKey()[0]=>SORT_DESC]);
+
+        return $dataProvider;
+    }
+}
---
+++ hospital/models/UserDoctorAppointSearchModels.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace hospital\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\UserDoctorAppoint;
+
+/**
+ * UserDoctorAppointSearchModels represents the model behind the search form about `\common\models\UserDoctorAppoint`.
+ */
+class UserDoctorAppointSearchModels extends UserDoctorAppoint
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['doctorid', 'weeks', 'cycle', 'delay', 'type1_num', 'type2_num', 'type3_num', 'type4_num', 'type5_num', 'type6_num'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = UserDoctorAppoint::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+        $doctorid=\common\models\UserDoctor::findOne(['hospitalid'=>\Yii::$app->user->identity->hospital]);
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'doctorid' =>$doctorid,
+            'weeks' => $this->weeks,
+            'cycle' => $this->cycle,
+            'delay' => $this->delay,
+            'type1_num' => $this->type1_num,
+            'type2_num' => $this->type2_num,
+            'type3_num' => $this->type3_num,
+            'type4_num' => $this->type4_num,
+            'type5_num' => $this->type5_num,
+            'type6_num' => $this->type6_num,
+        ]);
+        $query->orderBy([self::primaryKey()[0]=>SORT_DESC]);
+
+        return $dataProvider;
+    }
+}
