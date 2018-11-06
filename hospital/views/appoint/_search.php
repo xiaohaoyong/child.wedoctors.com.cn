e725b623b0977f499336820d34f8c0263d1a0460
xiaohaoyong <xiaohaoyong@126.com>
2018-11-06 15:03:13 +0800

    预约系统数据库以及model

--- hospital/views/appoint/_search.php
+++ hospital/views/appoint/_search.php
@@ -24,6 +24,7 @@ use yii\widgets\ActiveForm;
     <?php  echo $form->field($model, 'appoint_time')->dropDownList(\common\models\Appoint::$timeText,['prompt'=>'请选择']) ?>
 
     <?php  echo $form->field($model, 'state')->dropDownList(\common\models\Appoint::$stateText,['prompt'=>'请选择']) ?>
+    <?php  echo $form->field($model, 'type')->dropDownList(\common\models\Appoint::$typeText,['prompt'=>'请选择']) ?>
 
     <?php  echo $form->field($model, 'child_name') ?>
 
