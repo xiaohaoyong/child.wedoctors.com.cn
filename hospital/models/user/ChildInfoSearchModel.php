64eb58e1a7948e91bd40a12c42f71e4264ab8a5e
xiaohaoyong <xiaohaoyong@126.com>
2018-12-21 15:21:38 +0800

    今日签约

--- hospital/models/user/ChildInfoSearchModel.php
+++ hospital/models/user/ChildInfoSearchModel.php
@@ -20,7 +20,6 @@ class ChildInfoSearchModel extends ChildInfo
     public $docpartimeE;
 
 
-
     public $child_type;
 
     public $username;
@@ -35,8 +34,8 @@ class ChildInfoSearchModel extends ChildInfo
     public function rules()
     {
         return [
-            [['id', 'userid', 'birthday', 'createtime', 'level','admin','child_type'], 'integer'],
-            [['docpartimeS','docpartimeE','birthdayS','birthdayE', 'username'], 'string'],
+            [['id', 'userid', 'birthday', 'createtime', 'level', 'admin', 'child_type'], 'integer'],
+            [['docpartimeS', 'docpartimeE', 'birthdayS', 'birthdayE', 'username'], 'string'],
             [['userphone'], 'integer'],
 
             [['name'], 'safe'],
@@ -45,17 +44,19 @@ class ChildInfoSearchModel extends ChildInfo
 
     public function attributeLabels()
     {
-        return [
-            'level' => '签约状态',
-            'docpartimeS' => '签约时间',
-            'docpartimeE' => '~',
-            'birthdayS' => '出生日期',
-            'birthdayE' => '~',
-            'username' => '父母联系人姓名',
-            'admin'=>'管理机构',
-            'child_type' => '儿童年龄段',
-            'userphone' => '父母联系人手机号'
-        ];
+        $attr = parent::attributeLabels();
+
+
+        $attr['level'] = '签约状态';
+        $attr['docpartimeS'] = '签约时间';
+        $attr['docpartimeE'] = '~';
+        $attr['birthdayS'] = '出生日期';
+        $attr['birthdayE'] = '~';
+        $attr['username'] = '父母联系人姓名';
+        $attr['admin'] = '管理机构';
+        $attr['child_type'] = '儿童年龄段';
+        $attr['userphone'] = '父母联系人手机号';
+        return $attr;
     }
 
     /**
@@ -90,56 +91,55 @@ class ChildInfoSearchModel extends ChildInfo
             // $query->where('0=1');
             return $dataProvider;
         }
-        if($this->child_type){
+        if ($this->child_type) {
             $mouth = ChildInfo::getChildType($this->child_type);
             $query->andFilterWhere(['>=', 'child_info.birthday', $mouth['firstday']]);
             $query->andFilterWhere(['<=', 'child_info.birthday', $mouth['lastday']]);
         }
 
-        $hospitalid=$this->admin?$this->admin:\Yii::$app->user->identity->hospital;
-        $doctorid=\common\models\UserDoctor::findOne(['hospitalid'=>\Yii::$app->user->identity->hospital]);
+        $hospitalid = $this->admin ? $this->admin : \Yii::$app->user->identity->hospital;
+        $doctorid = \common\models\UserDoctor::findOne(['hospitalid' => \Yii::$app->user->identity->hospital]);
 
-        if(Yii::$app->user->identity->county == 1105){
-            $year=6;
-        }else{
-            $year=3;
+        if (Yii::$app->user->identity->county == 1105) {
+            $year = 6;
+        } else {
+            $year = 3;
         }
 
 
-
-        if($this->level==1){
+        if ($this->level == 1) {
             $query->leftJoin('doctor_parent', '`doctor_parent`.`parentid` = `child_info`.`userid`');
             $query->andFilterWhere(['`doctor_parent`.`doctorid`' => $doctorid->userid]);
             $query->andFilterWhere(['`doctor_parent`.`level`' => 1]);
-            $query->andFilterWhere(['child_info.admin'=>$hospitalid]);
+            $query->andFilterWhere(['child_info.admin' => $hospitalid]);
             $query->andFilterWhere(['>', '`child_info`.birthday', strtotime("-$year year")]);
             $query->andFilterWhere(['`child_info`.`doctorid`' => $hospitalid]);
-        }elseif($this->level==2){
+        } elseif ($this->level == 2) {
             $query->leftJoin('doctor_parent', '`doctor_parent`.`parentid` = `child_info`.`userid`');
             $query->andFilterWhere(['`doctor_parent`.`doctorid`' => $doctorid->userid]);
             $query->andFilterWhere(['`doctor_parent`.`level`' => 1]);
-            $query->andWhere(['`child_info`.`source`'=>0]);
-        }elseif($this->level==3){
-            $parentids=\common\models\DoctorParent::find()->select('parentid')->andFilterWhere(['`doctor_parent`.`doctorid`'=>$doctorid->userid])->andFilterWhere(['level'=>1])->column();
+            $query->andWhere(['`child_info`.`source`' => 0]);
+        } elseif ($this->level == 3) {
+            $parentids = \common\models\DoctorParent::find()->select('parentid')->andFilterWhere(['`doctor_parent`.`doctorid`' => $doctorid->userid])->andFilterWhere(['level' => 1])->column();
             $query->andFilterWhere(['>', '`child_info`.birthday', strtotime("-$year year")]);
-            $query->andFilterWhere(['not in', '`child_info`.userid',$parentids]);
+            $query->andFilterWhere(['not in', '`child_info`.userid', $parentids]);
             $query->andFilterWhere(['`child_info`.`admin`' => $hospitalid]);
-        }elseif($this->level==4){
+        } elseif ($this->level == 4) {
             $query->leftJoin('doctor_parent', '`doctor_parent`.`parentid` = `child_info`.`userid`');
             $query->andWhere(['<', '`child_info`.birthday', strtotime("-$year year")]);
-            $query->orWhere(['child_info.admin'=>0]);
+            $query->orWhere(['child_info.admin' => 0]);
 
-            $query->andWhere(['`child_info`.`source`'=>$hospitalid]);
+            $query->andWhere(['`child_info`.`source`' => $hospitalid]);
             $query->andWhere(['`doctor_parent`.`doctorid`' => $doctorid->userid]);
             $query->andWhere(['`doctor_parent`.`level`' => 1]);
-        }else{
+        } else {
             $query->andFilterWhere(['>', '`child_info`.birthday', strtotime("-$year year")]);
             $query->andFilterWhere(['`child_info`.`admin`' => $hospitalid]);
         }
 
 
-        if($this->docpartimeS!=='' and $this->docpartimeS!==null){
-            if($this->level!=1 && $this->level!=2) {
+        if ($this->docpartimeS !== '' and $this->docpartimeS !== null) {
+            if ($this->level != 1 && $this->level != 2) {
                 $query->leftJoin('doctor_parent', '`doctor_parent`.`parentid` = `child_info`.`userid`');
             }
             $state = strtotime($this->docpartimeS . " 00:00:00");
@@ -148,7 +148,7 @@ class ChildInfoSearchModel extends ChildInfo
             $query->andFilterWhere(['<', '`doctor_parent`.`createtime`', $end]);
         }
 
-        if($this->birthdayE!=='' and $this->birthdayS!==null){
+        if ($this->birthdayE !== '' and $this->birthdayS !== null) {
             $state = strtotime($this->birthdayS . " 00:00:00");
             $end = strtotime($this->birthdayE . " 23:59:59");
             $query->andFilterWhere(['>', '`birthday`', $state]);
@@ -159,16 +159,16 @@ class ChildInfoSearchModel extends ChildInfo
 
             $query->leftJoin('user_parent', '`user_parent`.`userid` = `child_info`.`userid`');
             if ($this->username) {
-                $query->andWhere(['or',['`user_parent`.`mother`' => $this->username],['`user_parent`.`father`' => $this->username],['`user_parent`.`field11`' => $this->username]]);
+                $query->andWhere(['or', ['`user_parent`.`mother`' => $this->username], ['`user_parent`.`father`' => $this->username], ['`user_parent`.`field11`' => $this->username]]);
             }
             if ($this->userphone) {
-                $query->andWhere(['or',['`user_parent`.`mother_phone`' => $this->userphone],['`user_parent`.`father_phone`' => $this->userphone],['`user_parent`.`field12`' => $this->userphone]]);
+                $query->andWhere(['or', ['`user_parent`.`mother_phone`' => $this->userphone], ['`user_parent`.`father_phone`' => $this->userphone], ['`user_parent`.`field12`' => $this->userphone]]);
             }
         }
-        if($this->level==1) {
+        if ($this->level == 1) {
             $query->orderBy('`doctor_parent`.createtime desc');
 
-        }else {
+        } else {
             $query->orderBy([self::primaryKey()[0] => SORT_DESC]);
         }
 
