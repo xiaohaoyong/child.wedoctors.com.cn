d63d77c257c30dd2fb498219e26bc1f7c4d8e29a
xiaohaoyong <xiaohaoyong@126.com>
2019-01-21 11:25:03 +0800

    孕产妇管理

--- api/controllers/NoticeController.php
+++ api/controllers/NoticeController.php
@@ -18,6 +18,7 @@ use common\models\Examination;
 use common\models\HospitalAppoint;
 use common\models\Notice;
 use common\models\ArticleComment;
+use common\models\Pregnancy;
 use common\models\UserDoctor;
 use common\models\UserDoctorAppoint;
 use common\models\UserParent;
@@ -38,7 +39,7 @@ class NoticeController extends Controller
 
             if ($childs) {
                 $ex = Examination::find()->select('field52,childid')->andFilterWhere(['in', 'childid', $childs])->all();
-                if($ex) {
+                if ($ex) {
                     foreach ($ex as $k => $v) {
                         $row[$v->childid] = $v->field52;
                     }
@@ -54,58 +55,72 @@ class NoticeController extends Controller
                 }
             }
             if ($exT) {
-                $ex_data=[
+                $ex_data = [
                     'title' => '近期宝宝体检通知',
                     'ftitle' => $exT['date'] . ' (建议结合体检社区医院的门诊时间）',
                     'id' => '/user/examination/index?id=' . $exT['id'],
-                    'temp'=>1
+                    'temp' => 1
                 ];
-                $child=ChildInfo::findOne($exT['id']);
-                if($child && $child->source) {
+                $child = ChildInfo::findOne($exT['id']);
+                if ($child && $child->source) {
                     $doctor = UserDoctor::findOne(['hospitalid' => $child->source]);
-                    if($doctor && $doctor->userid){
-                        $appoint=HospitalAppoint::findOne(['doctorid'=>$doctor->userid,'type'=>1]);
-                        if($appoint) {
+                    if ($doctor && $doctor->userid) {
+                        $appoint = HospitalAppoint::findOne(['doctorid' => $doctor->userid, 'type' => 1]);
+                        if ($appoint) {
                             $ex_data['id2'] = '/appoint/form?id=' . $doctor->userid . "&type=1";
-                            $ex_data['temp']=3;
+                            $ex_data['temp'] = 3;
                         }
                     }
                 }
 
-                Notice::setList($this->userid, 1,$ex_data , "id=" . $exT['id']);
+                Notice::setList($this->userid, 1, $ex_data, "id=" . $exT['id']);
             }
         }
 
         if ($this->version && $this->version >= '0.9.4') {
 
             $childInfo = ChildInfo::find()->andWhere(['userid' => $this->userid])->orderBy('birthday asc')->one();
-            $DiffDate = \common\helpers\StringHelper::DiffDate(date('Y-m-d', time()), date('Y-m-d', $childInfo->birthday));
-            $toolTag_tag=[];
-            if($DiffDate[0]) {
-                $toolTag_tag[0] = $DiffDate[0] . 'Y';
-            }else{
-                $d=ceil($DiffDate[2]/7);
-                $toolTag_tag[2] = ($d==0?1:$d)."W";
-            }
-            if ($DiffDate[1] && $DiffDate[0]<3) {
-                $toolTag_tag[1] = $DiffDate[1] . 'M';
-            }elseif($DiffDate[0]>2){
-                if($DiffDate[1]>=0 && $DiffDate[1]<3){
+            if ($childInfo) {
+                $DiffDate = \common\helpers\StringHelper::DiffDate(date('Y-m-d', time()), date('Y-m-d', $childInfo->birthday));
+                $toolTag_tag = [];
+                if ($DiffDate[0]) {
+                    $toolTag_tag[0] = $DiffDate[0] . 'Y';
+                } else {
+                    $d = ceil($DiffDate[2] / 7);
+                    $toolTag_tag[2] = ($d == 0 ? 1 : $d) . "W";
+                }
+                if ($DiffDate[1] && $DiffDate[0] < 3) {
+                    $toolTag_tag[1] = $DiffDate[1] . 'M';
+                } elseif ($DiffDate[0] > 2) {
+                    if ($DiffDate[1] >= 0 && $DiffDate[1] < 3) {
 
-                }elseif ($DiffDate[1]>=3 && $DiffDate[1]<6){
-                    $toolTag_tag[1] = '3M';
-                }elseif ($DiffDate[1]>=6 && $DiffDate[1]<9){
-                    $toolTag_tag[1] = '6M';
-                }else{
-                    $toolTag_tag[1] = '9M';
+                    } elseif ($DiffDate[1] >= 3 && $DiffDate[1] < 6) {
+                        $toolTag_tag[1] = '3M';
+                    } elseif ($DiffDate[1] >= 6 && $DiffDate[1] < 9) {
+                        $toolTag_tag[1] = '6M';
+                    } else {
+                        $toolTag_tag[1] = '9M';
+                    }
+                }
+
+                ksort($toolTag_tag);
+                $toolTag = implode("", $toolTag_tag);
+                $tagName = "宝宝";
+                $nametwo = "宝宝育儿指南";
+
+            } else {
+                $preg = Pregnancy::findOne(['familyid' => $this->userid]);
+                if ($preg) {
+                    $week = $preg->getWeeks();
+                    $toolTag = $week . "YW";
+                    $tagName = "妈妈";
+                    $nametwo = "";
                 }
             }
 
-            ksort($toolTag_tag);
-            $toolTag=implode("",$toolTag_tag);
 
-            if ($tag = BabyToolTag::find()->where(['tag' => $toolTag])->one()) {
-                $baby = BabyTool::findOne(['tag' => '宝宝', 'period' => $tag->id]);
+            if ($toolTag && $tag = BabyToolTag::find()->where(['tag' => $toolTag])->one()) {
+                $baby = BabyTool::findOne(['tag' => $tagName, 'period' => $tag->id]);
                 if ($tag->mother_num + $tag->father_num != 0) {
                     $mother_num = round($tag->mother_num / ($tag->mother_num + $tag->father_num), 2) * 100;
                     $father_num = round($tag->father_num / ($tag->mother_num + $tag->father_num), 2) * 100;
@@ -115,28 +130,30 @@ class NoticeController extends Controller
                 }
 
                 $like = BabyToolLike::find()->select('loginid')->where(['bid' => $tag->id])->andWhere(['type' => 3])->orderBy('id desc')->limit(2)->column();
-                if($like) {
+                if ($like) {
                     $userlist = WxInfo::find()->select('avatarUrl')->andFilterWhere(['in', 'loginid', $like])->column();
                 }
 
                 $likeCount = BabyToolLike::find()->where(['bid' => $tag->id])->andWhere(['type' => 3])->count();
-                $isCollection=BabyToolLike::findOne(['bid'=> $tag->id,'loginid'=>$this->userLogin->id,'type'=>2]);
+                $isCollection = BabyToolLike::findOne(['bid' => $tag->id, 'loginid' => $this->userLogin->id, 'type' => 2]);
 
                 Notice::setList($this->userid, 7, [
                     'userCount' => $likeCount,
                     'userlist' => $userlist,
                     'mother_num' => (int)$mother_num,
                     'father_num' => (int)$father_num,
-                    'value' => $tag->name . "宝宝育儿指南",
+                    'value' => $tag->name . $nametwo,
                     'content' => mb_substr($baby->content, 0, 100, 'utf-8'),
-                    'title' => $tag->name . "宝宝育儿指南",
+                    'title' => $tag->name . $nametwo,
                     'ftitle' => '家长必读',
                     'id' => '/tool/baby/index?catid=' . $tag->id,
                     'ids' => $tag->id,
                     'temp' => 2,
-                    'isCollection'=>$isCollection?1:0,
+                    'isCollection' => $isCollection ? 1 : 0,
                 ], '', '/tool/baby/index');
             }
+
+
         }
 
 
