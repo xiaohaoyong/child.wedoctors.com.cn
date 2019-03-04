88163894139003b0ab7061d94e79ed3888af5ed0
xiaohaoyong <xiaohaoyong@126.com>
2019-03-04 12:13:17 +0800

    调试

--- hospital/controllers/BaseController.php
+++ hospital/controllers/BaseController.php
@@ -7,7 +7,7 @@ class BaseController extends \yii\web\Controller {
     private $notCheckAccess = ['/rbac/access-error', 'site/index'];
 
     private $ignore = [
-        'site/login', 'site/logout','site/captcha','site/code'
+        'site/login', 'site/logout','site/captcha','site/code','site/code','synchronization/data-callback'
     ];
 
     public function beforeAction($action)
