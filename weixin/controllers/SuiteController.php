983ae90e8b3ee4fa925bfd87df1b044c9f5cf0c3
xiaohaoyong <xiaohaoyong@126.com>
2019-03-04 10:53:34 +0800

    调试

--- weixin/controllers/SuiteController.php
+++ weixin/controllers/SuiteController.php
@@ -159,13 +159,8 @@ class SuiteController extends Controller
         $this->mpWechat->createMenu(
             [
                 ['type' => 'miniprogram', 'name' => '育儿服务', 'url' =>'pages/index/index','appid'=>\Yii::$app->params['wxXAppId'],'pagepath'=>'pages/index/index',],
-                ['type' => 'miniprogram', 'name' => '育儿课堂', 'url' => 'pages/article/index/index','appid'=>\Yii::$app->params['wxXAppId'],'pagepath'=>'pages/article/index/index',],
+                ['type' => 'view', 'name' => '小课堂', 'url' => 'https://m.qlchat.com/wechat/page/live/2000001457808355'],
                 ['type' => 'view', 'name' => '我的', 'sub_button' =>[
-                    [
-                        'type'=>'view',
-                        'name'=>'小课堂',
-                        'url'=>'https://m.qlchat.com/wechat/page/live/2000001457808355',
-                    ],
                     [
                         'type'=>'miniprogram',
                         'name'=>'我的预约',
