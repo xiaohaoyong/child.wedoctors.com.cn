1c6ba5f97ffe286cba28254bfb4a1e8f02a9f21d
xiaohaoyong <xiaohaoyong@126.com>
2019-03-06 15:01:43 +0800

    异步导入数据

--- console/controllers/DataUpdateController.php
+++ console/controllers/DataUpdateController.php
@@ -8,6 +8,8 @@
 
 namespace console\controllers;
 
+use OSS\Core\OssException;
+use OSS\OssClient;
 use udokmeci\yii2beanstalk\BeanstalkController;
 
 class DataUpdateController extends BeanstalkController
@@ -34,6 +36,29 @@ class DataUpdateController extends BeanstalkController
         $log=new \common\components\Log('datacallback');
         $log->addLog('异步任务');
         $log->addLog(json_encode([$hospitalid,$date]));
+
+        $bucket= "wedoctorschild";
+// object 表示您在下载文件时需要指定的文件名称，如abc/efg/123.jpg。
+        $object = $date."-".$hospitalid.".xlsx";
+// 指定文件下载路径。
+        $localfile = "/tmp";
+        $options = array(
+            OssClient::OSS_FILE_DOWNLOAD => $localfile
+        );
+
+        try{
+            $ossClient = new OssClient('LTAIteFpOZnX3aoE', 'lYWI5AzSjQiZWBhC2d7Ttt06bnoDFF', 'oss-cn-beijing.aliyuncs.com');
+
+            $ossClient->getObject($bucket, $object, $options);
+        } catch(OssException $e) {
+            printf(__FUNCTION__ . ": FAILED\n");
+            printf($e->getMessage() . "\n");
+            return;
+        }
+        print(__FUNCTION__ . ": OK, please check localfile: 'upload-test-object-name.txt'" . "\n");
+
+
+
         $log->saveLog();
 
 
