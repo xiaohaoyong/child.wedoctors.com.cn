d0a543c3d0cb915ddb197e1054db50dc79cf2d75
xiaohaoyong <xiaohaoyong@126.com>
2019-03-12 17:31:07 +0800

    异步导入数据

--- hospital/views/layouts/left.php
+++ hospital/views/layouts/left.php
@@ -45,6 +45,7 @@
                             ['label' => '发布通知', 'url' => ['article/tongzhi']],
                             ['label' => '平台召回用户统计', 'url' => ['/push-log']],
                             ['label' => '同步已签约数据', 'url' => ['/synchronization']],
+                            ['label' => '数据同步（测试版）', 'url' => ['/synchronization/data']],
 
                         ]
                     ],
