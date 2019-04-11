05c840ad4c138cf4b86a28365bccb894c697b24f
xiaohaoyong <xiaohaoyong@126.com>
2019-04-11 10:48:56 +0800

    发送短信接口

--- interfaces/controllers/SmsController.php
+++ interfaces/controllers/SmsController.php
@@ -18,13 +18,13 @@ class SmsController extends Controller
         3=>'SMS_163056341',//支付成功
     ];
     //发送短信模板
-    public function actionPhoneDoctor($phone,$type){
+    public function actionPhoneDoctor($userphone,$type){
         $params=\Yii::$app->request->post();
         if(self::$type[$type]) {
             $response = \Yii::$app->aliyun->sendSms(
                 "儿宝宝", // 短信签名
                 self::$type[$type], // 短信模板编号
-                $phone, // 短信接收者
+                $userphone, // 短信接收者
                 $params
             );
         }
