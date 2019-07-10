1b8cf6d4156296c433685922f8b30f2ab08efcc1
xiaohaoyong <xiaohaoyong@126.com>
2019-07-10 12:38:30 +0800

    铁营

--- docapi/modules/v1/controllers/UserController.php
+++ docapi/modules/v1/controllers/UserController.php
@@ -12,6 +12,7 @@ namespace docapi\modules\v1\controllers;
 use common\models\Doctors;
 use common\models\Hospital;
 use common\models\UserDoctor;
+use common\models\UserLogin;
 
 class UserController extends \docapi\controllers\UserController
 {
@@ -21,6 +22,10 @@ class UserController extends \docapi\controllers\UserController
         $row=$userDoctor->toArray();
         $row['hospital']=Hospital::findOne(['id'=>$doctor->hospitalid])->name;
         return $row;
+    }
+    public function actionDoctor(){
+        $userLogin = UserLogin::findOne(['userid' => $this->userid]);
+        return $userLogin;
     }
 
 }
