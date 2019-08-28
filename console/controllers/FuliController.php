2098c68abb19d0d23de3e2185fa8ffd1f91c82e5
xiaohaoyong <xiaohaoyong@126.com>
2019-08-28 11:44:12 +0800

    丰台社区家庭医生团队

--- console/controllers/FuliController.php
+++ console/controllers/FuliController.php
@@ -45,7 +45,6 @@ class FuliController extends Controller
                 $rs = $app->customer_service
                     ->message('儿宝宝福利社上线啦，我们会定期给家长们搜罗些免费、实用、有趣的福利哦！' . "\n" . '<a href="http://child.wedoctors.com.cn" data-miniprogram-appid="wx6c33bfd66eb0a4f0" data-miniprogram-path="pages/index/index">快快来领取吧</a>')
                     ->to($v->openid)->send();
-                $return=1;
                 $pushLog=new PushLog();
                 $pushLog->openid=$v->openid;
                 $pushLog->type=1;
