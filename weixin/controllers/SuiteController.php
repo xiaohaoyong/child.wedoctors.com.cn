5d435754de976a6e15c40a420cb9aba957f6a867
xiaohaoyong <xiaohaoyong@126.com>
2019-11-11 17:57:47 +0800

    丰台社区家庭医生团队

--- weixin/controllers/SuiteController.php
+++ weixin/controllers/SuiteController.php
@@ -155,6 +155,7 @@ class SuiteController extends Controller
 
     //客服消息
     public function custom_send($openid){
+        sleep(1);
         $config = [
             'app_id' => 'wx1147c2e491dfdf1d',
             'secret' => '98001ba41e010dea2861f3e0d95cbb15',
