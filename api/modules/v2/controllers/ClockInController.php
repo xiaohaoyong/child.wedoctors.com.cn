9b629aede01d744d276d4e8faa5bcb3e56e17941
xiaohaoyong <xiaohaoyong@126.com>
2019-11-26 17:37:32 +0800

    积分

--- api/modules/v2/controllers/ClockInController.php
+++ api/modules/v2/controllers/ClockInController.php
@@ -78,7 +78,7 @@ class ClockInController extends Controller
 
 
 
-        return ['row'=>$row,'point'=>implode(',',$point),'total'=>$total];
+        return ['row'=>$row,'point'=>implode(',',$point),'total'=>$total?$total:0];
     }
 
     public function actionUser(){
