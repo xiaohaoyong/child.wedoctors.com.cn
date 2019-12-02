5f514f4cbcefa726c2cf34e335033ded5fe5e15b
xiaohaoyong <xiaohaoyong@126.com>
2019-12-02 17:34:18 +0800

    积分

--- api/controllers/Controller.php
+++ api/controllers/Controller.php
@@ -24,8 +24,8 @@ use yii\web\Response;
 
 class Controller extends \yii\web\Controller
 {
-    private $result = ['user/login','user/wx-user-info','article/view','baby/collection-list','text/text'];
-    private $autoResult=['user/save-image','user/login'];
+    private $result = ['user/login', 'user/wx-user-info', 'article/view', 'baby/collection-list', 'text/text'];
+    private $autoResult = ['user/save-image', 'user/login'];
     protected $userid = 0;
     protected $user;
     protected $seaver_token;
@@ -36,54 +36,55 @@ class Controller extends \yii\web\Controller
 
     public function beforeAction($action)
     {
-        \Yii::$app->response->format=Response::FORMAT_JSON;
+        \Yii::$app->response->format = Response::FORMAT_JSON;
 
-        $this->seaver_token=\Yii::$app->request->headers->get('sessionkey');
-        $this->hxusername=\Yii::$app->request->headers->get('hxusername');
-        $this->version=\Yii::$app->request->headers->get('version');
+        $this->seaver_token = \Yii::$app->request->headers->get('sessionkey');
+        $this->hxusername = \Yii::$app->request->headers->get('hxusername');
+        $this->version = \Yii::$app->request->headers->get('version');
 
-        $cache=\Yii::$app->rdmp;
-        $session=$cache->get($this->seaver_token);
-        $session=explode('@@',$session);
         $controllerID = \Yii::$app->controller->id;
         $actionID = \Yii::$app->controller->action->id;
 
-        if($this->seaver_token && $session[0])
-        {
-            $userLogin=UserLogin::findOne(['xopenid'=>$session[0]]);
-            if(!$userLogin && !in_array($controllerID."/".$actionID,$this->result)){
-                $cache=\Yii::$app->rdmp;
-                $cache->lpush("user_login_error",$session[0]);
-                \Yii::$app->response->data = ['code' => 30001, 'msg' => '未授权访问'];
-                return false;
-            }
-            $this->userid=$userLogin->userid;
-            $this->user=$userLogin->user;
-            $this->appToken=$session;
-            $this->userLogin=$userLogin;
+        if ($this->seaver_token) {
+            $cache = \Yii::$app->rdmp;
+            $session = $cache->get($this->seaver_token);
+            $session = explode('@@', $session);
+            if ($session[0]) {
+                $userLogin = UserLogin::findOne(['xopenid' => $session[0]]);
+                if (!$userLogin && !in_array($controllerID . "/" . $actionID, $this->result)) {
+                    $cache = \Yii::$app->rdmp;
+                    $cache->lpush("user_login_error", $session[0]);
+                    \Yii::$app->response->data = ['code' => 30001, 'msg' => '未授权访问'];
+                    return false;
+                }
+                $this->userid = $userLogin->userid;
+                $this->user = $userLogin->user;
+                $this->appToken = $session;
+                $this->userLogin = $userLogin;
 
-            //判断是否签名
-            if($this->userid && !in_array($controllerID."/".$actionID,$this->autoResult)){
-                $doctorParent=DoctorParent::findOne(['parentid'=>$this->userid]);
-                if($doctorParent) {
-                    $doctor = UserDoctor::findOne(['userid' => $doctorParent->doctorid]);
-                    if(in_array($doctor->county,[1106,1105]) || in_array($doctorParent->doctorid,[143296,118080,126118,126122,160226,113896,113478,91722,175877,176156,184741,184793,190922,192821,194606,206259,206262,206260,213579,213390,213581,216592,216593,216594,219333,221895,223413,228039,240074,240185,240188,240189,240191,248035,248033])) {
-                        $auto = Autograph::findOne(['userid' => $this->userid]);
-                        if (!$auto) {
-                            //判断是否添加了宝宝或者孕产妇
-                            $child=ChildInfo::findOne(['userid'=>$this->userid]);
-                            $preg=Pregnancy::findOne(['familyid'=>$this->userid]);
-                            if($child || $preg) {
-                                \Yii::$app->response->data = ['code' => 30002, 'msg' => '已签约未签字'];
-                                return false;
+                //判断是否签名
+                if ($this->userid && !in_array($controllerID . "/" . $actionID, $this->autoResult)) {
+                    $doctorParent = DoctorParent::findOne(['parentid' => $this->userid]);
+                    if ($doctorParent) {
+                        $doctor = UserDoctor::findOne(['userid' => $doctorParent->doctorid]);
+                        if (in_array($doctor->county, [1106, 1105]) || in_array($doctorParent->doctorid, [143296, 118080, 126118, 126122, 160226, 113896, 113478, 91722, 175877, 176156, 184741, 184793, 190922, 192821, 194606, 206259, 206262, 206260, 213579, 213390, 213581, 216592, 216593, 216594, 219333, 221895, 223413, 228039, 240074, 240185, 240188, 240189, 240191, 248035, 248033])) {
+                            $auto = Autograph::findOne(['userid' => $this->userid]);
+                            if (!$auto) {
+                                //判断是否添加了宝宝或者孕产妇
+                                $child = ChildInfo::findOne(['userid' => $this->userid]);
+                                $preg = Pregnancy::findOne(['familyid' => $this->userid]);
+                                if ($child || $preg) {
+                                    \Yii::$app->response->data = ['code' => 30002, 'msg' => '已签约未签字'];
+                                    return false;
+                                }
                             }
                         }
                     }
                 }
             }
 
-        }elseif(!in_array($controllerID."/".$actionID,$this->result)){
-            \Yii::$app->response->data = ['code' => 30001,'msg' => '数字签证错误'];
+        } elseif (!in_array($controllerID . "/" . $actionID, $this->result)) {
+            \Yii::$app->response->data = ['code' => 30001, 'msg' => '数字签证错误'];
             return false;
         }
 
@@ -105,17 +106,16 @@ class Controller extends \yii\web\Controller
     {
         $return = parent::afterAction($action, $result); // TODO: Change the autogenerated
 
-        $return=$this->return_array_filter($return);
-        if($result instanceof Code && $result->getCode())
-        {
+        $return = $this->return_array_filter($return);
+        if ($result instanceof Code && $result->getCode()) {
             return $result->getCode();
         }
 
-        $this->result=Code::result();
+        $this->result = Code::result();
         if (!isset($return['data']) && $return) {
             $this->result['data'] = $return;
-        }elseif (is_array($return)) {
-            if($return) {
+        } elseif (is_array($return)) {
+            if ($return) {
                 foreach ($return as $k => $v) {
                     if ($k == 'data' || $k == 'msg' || $k == 'code') {
                         $this->result[$k] = $v;
@@ -123,7 +123,7 @@ class Controller extends \yii\web\Controller
                         $this->result['data'][$k] = $v;
                     }
                 }
-            }else{
+            } else {
                 $this->result['data'] = [];
             }
         } else {
@@ -134,17 +134,15 @@ class Controller extends \yii\web\Controller
 
     private function return_array_filter($return)
     {
-        if(!is_array($return)) return $return;
-        $data=[];
-        foreach($return as $k=>$v)
-        {
-            if(is_array($v))
-            {
-                $row=$this->return_array_filter($v);
-            }else{
-                $row= $v===null?"":$v;
+        if (!is_array($return)) return $return;
+        $data = [];
+        foreach ($return as $k => $v) {
+            if (is_array($v)) {
+                $row = $this->return_array_filter($v);
+            } else {
+                $row = $v === null ? "" : $v;
             }
-            $data[$k]=$row;
+            $data[$k] = $row;
         }
         return $data;
     }
