b1fd02be9b1727454812ab62d5f15ae0b3721432
xiaohaoyong <xiaohaoyong@126.com>
2019-12-02 09:46:32 +0800

    积分

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -72,8 +72,8 @@ class UserController extends Controller
             $userJson = $curl->get();
             $log->addLog("wxrequist:".$userJson);
             $user = json_decode($userJson, true);
-            $log->addLog($user['errcode']);
-            if ($user['errcode']) {
+            $log->addLog($user);
+            if ($user['errcode'] || !$user) {
                 //获取用户微信登陆信息
                 $log->addLog("false1");
                 $path = "/sns/jscode2session?appid=" . \Yii::$app->params['wxXAppId'] . "&secret=" . \Yii::$app->params['wxXAppSecret'] . "&js_code=" . $code . "&grant_type=authorization_code";
