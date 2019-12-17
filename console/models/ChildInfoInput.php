681077eefddabc50994eb2bf18d6432d08405fbb
xiaohaoyong <xiaohaoyong@126.com>
2019-12-17 15:02:39 +0800

    续约通知

--- console/models/ChildInfoInput.php
+++ console/models/ChildInfoInput.php
@@ -54,7 +54,7 @@ class ChildInfoInput
             $value['field12'] = $field12[0];
         }
 
-        $this->log= new Log('childInfoUpdate');
+        $this->log= new Log('childInfoUpdate',true);
 
         if($hospitalid) {
             $this->hospitalid = $hospitalid;
