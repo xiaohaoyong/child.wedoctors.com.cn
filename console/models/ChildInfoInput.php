a1d21a2fd9d97b473f2eb0062e7ce5f2f4d849dd
xiaohaoyong <xiaohaoyong@126.com>
2019-12-17 15:11:04 +0800

    续约通知

--- console/models/ChildInfoInput.php
+++ console/models/ChildInfoInput.php
@@ -162,6 +162,7 @@ class ChildInfoInput
             $childData['source']=$this->hospitalid;
             $childData['admin']=$this->hospitalid;
 
+            $this->log->addLog("保存父母1");
             $child->load(['ChildInfo'=>$childData]);
             $this->log->addLog("保存儿童");
             if (!$child->save()) {
