7dfac6110730d4b68865e8a543ceb81845a8139d
xiaohaoyong <xiaohaoyong@126.com>
2020-02-03 14:08:51 +0800

    公众号自动回复

--- common/components/UploadForm.php
+++ common/components/UploadForm.php
@@ -44,7 +44,7 @@ class UploadForm extends Model
                 $file->saveAs(__ROOT__."/../../".\Yii::$app->params['imageDir']."/upload/" .$filen. '.' . $file->extension);
 
                 try{
-                    $ossClient = new OssClient('LTAIteFpOZnX3aoE', 'lYWI5AzSjQiZWBhC2d7Ttt06bnoDFF', 'oss-cn-beijing.aliyuncs.com');
+                    $ossClient = new OssClient('LTAIteFpOZnX3aoE', 'lYWI5AzSjQiZWBhC2d7Ttt06bnoDFF', 'oss-cn-qingdao.aliyuncs.com');
 
                     $ossClient->putObject('childimage', $filen. '.' . $file->extension, '123123');
                 } catch(OssException $e) {
