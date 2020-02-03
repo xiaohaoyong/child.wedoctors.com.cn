cb1ae0a938e3da05f3faeedbb18050ec55fbad96
xiaohaoyong <xiaohaoyong@126.com>
2020-02-03 13:33:01 +0800

    公众号自动回复

--- common/components/UploadForm.php
+++ common/components/UploadForm.php
@@ -7,6 +7,8 @@
  */
 
 namespace common\components;
+use OSS\Core\OssException;
+use OSS\OssClient;
 use yii\base\Model;
 use yii\web\UploadedFile;
 
@@ -39,7 +41,19 @@ class UploadForm extends Model
                 $time=time();
                 $filen=substr(md5($time.$file->tempName),4,14);
                 $images[]=\Yii::$app->params['imageUrl'].$filen. '.' . $file->extension;
-                $file->saveAs(__ROOT__."/../../".\Yii::$app->params['imageDir']."/upload/" .$filen. '.' . $file->extension);
+                //$file->saveAs(__ROOT__."/../../".\Yii::$app->params['imageDir']."/upload/" .$filen. '.' . $file->extension);
+
+                try{
+                    $ossClient = new OssClient('LTAIteFpOZnX3aoE', 'lYWI5AzSjQiZWBhC2d7Ttt06bnoDFF', 'oss-cn-beijing.aliyuncs.com');
+
+                    $ossClient->uploadFile('childimage', "/upload/".$filen. '.' . $file->extension, $file->tempName);
+                } catch(OssException $e) {
+                    $log=new Log('uploadimg');
+                    $log->addLog($e->getMessage());
+                    $log->saveLog();
+                }
+
+
             }
             return $images;
         } else {
