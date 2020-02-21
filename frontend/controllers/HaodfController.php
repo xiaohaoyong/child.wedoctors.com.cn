c00d3909caa987be2f6abce0c31690be9a85f153
xiaohaoyong <xiaohaoyong@126.com>
2020-02-21 16:16:15 +0800

    公众号自动回复

--- frontend/controllers/HaodfController.php
+++ frontend/controllers/HaodfController.php
@@ -16,17 +16,17 @@ class HaodfController extends Controller
 {
     public function actionIndex(){
 
-        $config = [
-            // ...
-        ];
+
 
         $app = Factory::officialAccount(\Yii::$app->params['easywechat']);
         $oauth = $app->oauth;
-
+        if($oauth) {
 // 获取 OAuth 授权结果用户信息
-        $user = $oauth->user();
+            $user = $oauth->user();
 
+        }
         var_dump($user);exit;
-        
+
+        echo "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1147c2e491dfdf1d&redirect_uri=http://child.wedoctors.com.cn/haodf&response_type=code&scope=SCOPE&state=STATE#wechat_redirec";
     }
 }
