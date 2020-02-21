ef8df2960421fba15808054cf62677425e5fef2f
xiaohaoyong <xiaohaoyong@126.com>
2020-02-21 18:13:37 +0800

    公众号自动回复

--- frontend/controllers/HaodfController.php
+++ frontend/controllers/HaodfController.php
@@ -35,7 +35,7 @@ class HaodfController extends Controller
                     $params['partnerUserId']=$weopenid->id;
                     $signature = $this->generateSignature($secret, $timestamp, $partnerKey, $params);
                     $jumpUrl='https://m.haodf.com/ndynamic/coronalactivity/activity?businesstype=ebb';
-                    $url= "https://m.haodf.com/openplatform/auth?partnerKey={$partnerKey}&timestamp={$timestamp}&signature={$signature}&partnerUserId={$params['partnerUserId']}&jumpUrl={$jumpUrl}";
+                    $url= urlencode("https://m.haodf.com/openplatform/auth?partnerKey={$partnerKey}&timestamp={$timestamp}&signature={$signature}&partnerUserId={$params['partnerUserId']}&jumpUrl={$jumpUrl}");
                     return $this->redirect($url);
                 }
             }
