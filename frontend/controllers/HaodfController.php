b3cff7e517cb8c9dd81b3d09c199a4a0b5359155
xiaohaoyong <xiaohaoyong@126.com>
2020-02-21 17:33:54 +0800

    公众号自动回复

--- frontend/controllers/HaodfController.php
+++ frontend/controllers/HaodfController.php
@@ -9,6 +9,7 @@
 namespace frontend\controllers;
 
 
+use common\models\WeOpenid;
 use yii\web\Controller;
 use EasyWeChat\Factory;
 
@@ -24,10 +25,35 @@ class HaodfController extends Controller
 // 获取 OAuth 授权结果用户信息
             $user = $oauth->user();
             $userInfo=$user->toArray();
-            var_dump($userInfo['original']['openid']);
+            if($userInfo['original']['openid']){
+                $openid=$userInfo['original']['openid'];
+                $weopenid=WeOpenid::findOne(['openid'=>$openid]);
+                if($weopenid){
+                    $timestamp = time();
+                    $partnerKey="15f23dbae71f0f62";
+                    $secret="EfPqDznSfV";
+                    $params['partnerUserId']=$weopenid->id;
+                    $signature = generateSignature($secret, $timestamp, $partnerKey, $params);
+                    $url= "https://m.haodf.com/openplatform/auth?partnerKey={$partnerKey}&timestamp={$timestamp}&signature={$signature}&partnerUserId={$params['partnerUserId']}";
+                    $this->redirect($url);
+                }
+            }
             exit;
         }
-
-        echo "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1147c2e491dfdf1d&redirect_uri=http://web.child.wedoctors.com.cn/haodf&response_type=code&scope=snsapi_base&state=STATE#wechat_redirec";
+        $jumpUrl='https://m.haodf.com/ndynamic/coronalactivity/activity?businesstype=ebb';
+        echo "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx1147c2e491dfdf1d&redirect_uri=http://web.child.wedoctors.com.cn/haodf&response_type=code&scope=snsapi_base&state=STATE#wechat_redirec&jumpUrl={$jumpUrl}";
     }
+    public function generateSignature($secret, $timestamp, $partnerKey, $params) {
+        $paramArray = array();
+        foreach ($params as $param) {
+            $paramArray[] = strval($param);
+        }
+        $paramArray[] = strval($secret);
+        $paramArray[] = strval($timestamp);
+        $paramArray[] = strval($partnerKey);
+        sort($paramArray, SORT_STRING);
+        $paramString = implode('', $paramArray);
+        return sha1($paramString);
+    }
+
 }
