e8be6e7315849152b824e99489911a745755b936
xiaohaoyong <xiaohaoyong@126.com>
2020-03-25 17:10:33 +0800

    公众号自动回复

--- frontend/views/doctors/list.php
+++ frontend/views/doctors/list.php
@@ -29,7 +29,8 @@ $this->title = "社区医院查询"
                 height: 44px;
                 line-height: 44px;
                 width: 90%;
-                border: none;
+                outline: none;
+                border-radius: 0;
             }
 
             .box {
@@ -41,16 +42,40 @@ $this->title = "社区医院查询"
             .dropdown-toggle {
                 border: none
             }
-            .list{margin-top: 20px;}
-            .list .item{display: flex;border: 1px solid #F7F7F7; margin-bottom: 10px;}
-            .list .item .hospital{padding-top: 19px; padding-bottom: 10px; display: flex;flex-direction: column;justify-content: space-between;}
-            .list .item .hospital .name{font-size: 16px; color: #333333}
-            .list .item .hospital .enlarge{
+
+            .list {
+                margin-top: 20px;
+            }
+
+            .list .item {
+                display: flex;
+                border: 1px solid #F7F7F7;
+                margin-bottom: 10px;
+            }
+
+            .list .item .hospital {
+                padding-top: 19px;
+                padding-bottom: 10px;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+            }
+
+            .list .item .hospital .name {
+                font-size: 16px;
+                color: #333333
+            }
+
+            .list .item .hospital .enlarge {
                 background: url("/img/enlarge.png") no-repeat center left;
                 background-size: 16px 16px;
                 padding-left: 20px;
             }
-            .fo{text-align: center;line-height: 40px;}
+
+            .fo {
+                text-align: center;
+                line-height: 40px;
+            }
         </style>
     </head>
     <body class="hold-transition skin-green">
@@ -60,20 +85,20 @@ $this->title = "社区医院查询"
             <div class="search">
 
                 <form method="get">
-                <input name="search" placeholder="搜索社区">
+                    <input type="search" name="search" placeholder="搜索社区">
                 </form>
             </div>
             <div class="box">
                 <div class="dropdown">
                     <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                        <?=$county?\common\models\Area::$all[$county]:"选择区/县"?>
+                        <?= $county ? \common\models\Area::$all[$county] : "选择区/县" ?>
                         <span class="caret"></span>
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                        <?php foreach (\common\models\Area::$county['11'] as $k=>$v){?>
-                        <li><a href="?county=<?=$k?>"><?=$v?></a></li>
-                        <?php }?>
+                        <?php foreach (\common\models\Area::$county['11'] as $k => $v) { ?>
+                            <li><a href="?county=<?= $k ?>"><?= $v ?></a></li>
+                        <?php } ?>
 
                     </ul>
                 </div>
@@ -82,11 +107,13 @@ $this->title = "社区医院查询"
                 <?php
                 foreach ($doctors as $k => $v) {
                     ?>
-                    <div class="item" data-toggle="modal" data-target="#create-modal<?=$v['userid']?>">
-                        <div class="qrcode"><img src="<?=\yii\helpers\Url::to('doctors/qr?text='.urlencode($v['qrcode']))?>"/></div>
+                    <div class="item" data-toggle="modal" data-target="#create-modal<?= $v['userid'] ?>">
+                        <div class="qrcode"><img
+                                    src="<?= \yii\helpers\Url::to('doctors/qr?text=' . urlencode($v['qrcode'])) ?>"/>
+                        </div>
                         <div class="hospital">
-                            <div class="name"><?=$v['name']?></div>
-                            <div class="address">社区电话：<?=$v['phone']?></div>
+                            <div class="name"><?= $v['name'] ?></div>
+                            <div class="address">社区电话：<?= $v['phone'] ?></div>
                             <div class="enlarge">点击放大识别二维码</div>
                         </div>
                     </div>
@@ -94,10 +121,11 @@ $this->title = "社区医院查询"
 
                     <?php
                     \yii\bootstrap\Modal::begin([
-                        'id' => 'create-modal'.$v['userid'],
+                        'id' => 'create-modal' . $v['userid'],
                     ]);
                     ?>
-                    <img src="<?=\yii\helpers\Url::to('doctors/qr?text='.urlencode($v['qrcode']))?>" width="320" height="320"/>
+                    <img src="<?= \yii\helpers\Url::to('doctors/qr?text=' . urlencode($v['qrcode'])) ?>" width="320"
+                         height="320"/>
                     <?php
                     \yii\bootstrap\Modal::end();
                     ?>
