627e3de614dde6ddb350f86b60f92cf4c5a4ca95
xiaohaoyong <xiaohaoyong@126.com>
2020-04-26 09:56:10 +0800

    成人疫苗

--- frontend/views/layouts/h5.php
+++ frontend/views/layouts/h5.php
@@ -18,7 +18,7 @@ $this->title = "预约成人疫苗"
     <?php $this->beginBody() ?>
     <?= \common\widgets\Alert::widget() ?>
     <?=$content?>
-    <div class="appoint_my"><a href="/wappoint/my"><img src="/img/appoint_my.png" width="50" height="50"></a></div>
+    <div class="appoint_my"><a href="/wappoint/my"><img src="/img/appoint_my.png" width="56" height="56"></a></div>
     <?php $this->endBody() ?>
 
     </body>
