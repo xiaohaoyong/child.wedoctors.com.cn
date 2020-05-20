1dacc2ff3e2b0c09b8e1dd23fddbef9a6ce15144
xiaohaoyong <xiaohaoyong@126.com>
2020-05-20 15:30:38 +0800

    宝宝添加失败

--- frontend/views/wappoint/from.php
+++ frontend/views/wappoint/from.php
@@ -138,7 +138,7 @@ jQuery("#appoint_phone").submit(phone_data,function(e){
 			return false;
 		}
 		if(this.name=='phone'){
-		    var reg =/^0?1[3|4|5|6|7|8][0-9]\d{8}$/;
+		    var reg =/^0?1[3|4|5|6|7|8|9][0-9]\d{8}$/;
             if(!reg.test(this.value)){
                 label = labelMap[this.name];
 			    return false;
