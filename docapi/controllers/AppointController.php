7bc28bff700fcade8b3036dc031b169b7560ce06
xiaohaoyong <xiaohaoyong@126.com>
2020-06-08 17:01:20 +0800

    疫苗预约列表优化

--- docapi/controllers/AppointController.php
+++ docapi/controllers/AppointController.php
@@ -17,27 +17,30 @@ use common\models\UserDoctor;
 class AppointController extends Controller
 {
     public function actionView($id){
-        $appoint=Appoint::findOne(['id'=>$id]);
+        $appoint=Appoint::findOne(['id'=>$id,'doctorid'=>$this->userid]);
+        $row=[];
+        if($appoint) {
 
-        $row=$appoint->toArray();
-        $doctor=UserDoctor::findOne(['userid'=>$appoint->doctorid]);
-        if($doctor){
-            $hospital=Hospital::findOne($doctor->hospitalid);
+            $row = $appoint->toArray();
+            $doctor = UserDoctor::findOne(['userid' => $appoint->doctorid]);
+            if ($doctor) {
+                $hospital = Hospital::findOne($doctor->hospitalid);
+            }
+            $row['hospital'] = $hospital->name;
+            $row['type'] = Appoint::$typeText[$appoint->type];
+            $row['time'] = date('Y.m.d', $appoint->appoint_date) . "  " . Appoint::$timeText[$appoint->appoint_time];
+            $row['child_name'] = ChildInfo::findOne($appoint->childid)->name;
+            $row['duan'] = $appoint->appoint_time;
+
+            $index = Appoint::find()
+                ->andWhere(['appoint_date' => $appoint->appoint_date])
+                ->andWhere(['<', 'id', $id])
+                ->andWhere(['doctorid' => $appoint->doctorid])
+                ->andWhere(['appoint_time' => $appoint->appoint_time])
+                ->andWhere(['type' => $appoint->type])
+                ->count();
+            $row['index'] = $index + 1;
         }
-        $row['hospital']=$hospital->name;
-        $row['type']=Appoint::$typeText[$appoint->type];
-        $row['time']=date('Y.m.d',$appoint->appoint_date)."  ".Appoint::$timeText[$appoint->appoint_time];
-        $row['child_name']=ChildInfo::findOne($appoint->childid)->name;
-        $row['duan']=$appoint->appoint_time;
-
-        $index=Appoint::find()
-            ->andWhere(['appoint_date'=>$appoint->appoint_date])
-            ->andWhere(['<','id',$id])
-            ->andWhere(['doctorid'=>$appoint->doctorid])
-            ->andWhere(['appoint_time'=>$appoint->appoint_time])
-            ->andWhere(['type' => $appoint->type])
-            ->count();
-        $row['index']=$index+1;
         return $row;
     }
 
