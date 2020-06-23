b9b9c097c64d74d8b294f89a20f0bfda80b7a292
xiaohaoyong <xiaohaoyong@126.com>
2020-06-23 10:36:54 +0800

    后台增加区县筛选

--- console/controllers/AppointController.php
+++ console/controllers/AppointController.php
@@ -43,7 +43,7 @@ class AppointController extends Controller
                         'keyword2' => ARRAY('value' => '您预约了' . date('Y年m月d', $day) . ' ' . Appoint::$typeText[$v->type] . '，请按照预约时间到达社区'),
                         'remark' => ARRAY('value' => "鉴于目前北京市二级防控阶段，请点击此通知填写流行病学调查表，根据不同社区工作安排可能需要您出示调查结果，调查结果可以在公众号底部菜单我的->流行病学调查表中查看", 'color' => '#221d95'),
                     ];
-                    if(in_array($v->doctorid,[216594])){
+                    if(in_array($v->doctorid,[216594,221895])){
                         $data['remark']='鉴于目前北京市二级防控阶段，为了您的健康，请您出门带好口罩做好预防措施';
                         $rs = WechatSendTmp::send($data, $openid, $temp, '',['appid' => \Yii::$app->params['wxXAppId'], 'pagepath' => 'pages/appoint/view?id='.$v->id,]);
                     }else{
