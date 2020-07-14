1a243a72e5a6e7e2439e6fb9e9e86778ed24eec6
xiaohaoyong <xiaohaoyong@126.com>
2020-07-14 15:36:57 +0800

    后台增加区县筛选

--- console/controllers/GeneratedController.php
+++ console/controllers/GeneratedController.php
@@ -16,7 +16,7 @@ class GeneratedController extends Controller
     public function actionQuestion1(){
         $start=strtotime(date('Ymd'));
         $end=strtotime(date('Ymd235959'));
-        $quaa=QuestionNaireAnswer::find()->select('userid,createtime,doctorid')->groupBy('userid,createtime')->orderBy('createtime desc')->count();
+        $quaa=QuestionNaireAnswer::find()->select('userid,createtime,doctorid')->groupBy('userid,createtime')->orderBy('createtime desc')->all();
         foreach($quaa as $k=>$v){
             system("wkhtmltopdf 'http://web.child.wedoctors.com.cn/question-naire/new-view?id=1&time=".$v->createtime."&userid=".$v->userid."' /tmp/Question1.pdf\n");
             system("ossutil64 cp /tmp/Question1.pdf oss://webdoctorsquestion/".$v->doctorid."/".date('Ymd',$v->createtime)."/".$v->userid.".pdf -f");
