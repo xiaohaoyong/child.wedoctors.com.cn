172082a57c624367954a628b804c8465191e84ec
xiaohaoyong <xiaohaoyong@126.com>
2020-07-16 00:44:49 +0800

    后台增加区县筛选

--- hospital/views/yiqing/down.php
+++ hospital/views/yiqing/down.php
@@ -10,8 +10,19 @@
 $this->title='疫情调查表';
 $userDoctor=\common\models\UserDoctor::findOne(['hospitalid'=>Yii::$app->user->identity->hospitalid]);
 $doctorid=$userDoctor->userid;
-
-echo \yii\helpers\Html::a('下载全部','http://static.i.wedoctors.com.cn/down/'.$doctorid.'.zip');
 ?>
-<hr>
-//筛选下载功能开发中。。。
+
+
+<div class="appoint-index">
+    <div class="col-xs-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <?=\yii\helpers\Html::a('下载全部','http://static.i.wedoctors.com.cn/down/'.$doctorid.'.zip');?>
+                <!-- /.box-tools -->
+                <hr>
+                //筛选下载功能开发中。。。
+            </div>
+        </div>
+    </div>
+</div>
+?>
