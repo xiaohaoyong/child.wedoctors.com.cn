382e5ca447907e8aceb8d582ef80034d572ab5eb
xiaohaoyong <xiaohaoyong@126.com>
2020-07-16 00:37:40 +0800

    后台增加区县筛选

--- hospital/views/yiqing/down.php
+++ hospital/views/yiqing/down.php
@@ -6,7 +6,7 @@
  * Time: 上午12:28
  */
 $doctorid=Yii::$app->user->identity->userid;
-echo \yii\helpers\Html::a('下载全部','http://static.i.wedoctors.com.cn/down/'.$doctorid.'zip');
+echo \yii\helpers\Html::a('下载全部','http://static.i.wedoctors.com.cn/down/'.$doctorid.'.zip');
 ?>
 <hr>
 //筛选下载功能开发中。。。
