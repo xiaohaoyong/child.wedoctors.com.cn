f7b72ff1b77a2d0c396a7eba357afb6d7f44ff88
xiaohaoyong <xiaohaoyong@126.com>
2020-07-16 00:38:13 +0800

    后台增加区县筛选

--- hospital/views/yiqing/down.php
+++ hospital/views/yiqing/down.php
@@ -5,6 +5,9 @@
  * Date: 2020/7/16
  * Time: 上午12:28
  */
+
+
+$this->title='疫情调查表';
 $doctorid=Yii::$app->user->identity->userid;
 echo \yii\helpers\Html::a('下载全部','http://static.i.wedoctors.com.cn/down/'.$doctorid.'.zip');
 ?>
