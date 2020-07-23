0b25f20f55301c04d93a09578d08fef0456b5c20
xiaohaoyong <xiaohaoyong@126.com>
2020-07-23 10:30:21 +0800

    大红门木材厂线下叫号机接口

--- hospital/views/layouts/left.php
+++ hospital/views/layouts/left.php
@@ -2,18 +2,18 @@
 
     <section class="sidebar">
 
-        <?php if (in_array(Yii::$app->user->identity->county, [1105, 1106,1109]) || in_array(Yii::$app->user->identity->hospitalid, [110587,110568,110586, 110582, 110583, 110584, 110589, 110571, 110590, 110591, 110593, 110592, 110594, 110595, 110596, 110597, 110598, 110599, 110602, 110601, 110603, 110604, 110605, 110606, 110607, 110608, 110609, 110610, 110613, 110614, 110615, 110616, 110617, 110618, 110620,110624,110625,110626,110627,110628,110629,110630,110632,110634])) {
+        <?php if (in_array(Yii::$app->user->identity->county, [1105, 1106, 1109]) || in_array(Yii::$app->user->identity->hospitalid, [110587, 110568, 110586, 110582, 110583, 110584, 110589, 110571, 110590, 110591, 110593, 110592, 110594, 110595, 110596, 110597, 110598, 110599, 110602, 110601, 110603, 110604, 110605, 110606, 110607, 110608, 110609, 110610, 110613, 110614, 110615, 110616, 110617, 110618, 110620, 110624, 110625, 110626, 110627, 110628, 110629, 110630, 110632, 110634])) {
             $a = ['label' => '儿童家医签约记录', 'icon' => 'pencil-square-o', 'url' => \yii\helpers\Url::to(['autograph/index']),];
             $b = ['label' => '孕产期管理', 'icon' => 'heart', 'url' => '#',
                 'items' => [
                     ['label' => '追访记录', 'url' => ['interview/index']],
-                    ['label' => '孕产期家医签约记录', 'url' => ['autograph/index','AutographSearch[t]'=>1]],
+                    ['label' => '孕产期家医签约记录', 'url' => ['autograph/index', 'AutographSearch[t]' => 1]],
                     ['label' => '孕产妇数据', 'url' => ['/pregnancy']],
                 ]
             ];
-        }elseif(in_array(Yii::$app->user->identity->hospitalid,[110578,110581])){
+        } elseif (in_array(Yii::$app->user->identity->hospitalid, [110578, 110581])) {
             $a = ['label' => '儿童家医签约记录', 'icon' => 'pencil-square-o', 'url' => \yii\helpers\Url::to(['autograph/index']),];
-        }elseif(in_array(Yii::$app->user->identity->hospitalid,[110543,110552])){
+        } elseif (in_array(Yii::$app->user->identity->hospitalid, [110543, 110552])) {
             $b = ['label' => '孕产期管理', 'icon' => 'heart', 'url' => '#',
                 'items' => [
                     ['label' => '追访记录', 'url' => ['interview/index']],
@@ -21,10 +21,15 @@
                 ]
             ];
         }
-        if(Yii::$app->user->identity->hospitalid!=110587){
-            $c= ['label' => '数据同步（测试版）', 'url' => ['/synchronization/data']];
-            $d= ['label' => '指导文章列表', 'url' => ['article/zindex?ArticleSearchModel[type]=1']];
-            $e= ['label' => '同步已签约数据', 'url' => ['/synchronization']];
+        if (Yii::$app->user->identity->hospitalid != 110587) {
+            $c = ['label' => '数据同步（测试版）', 'url' => ['/synchronization/data']];
+            $d = ['label' => '中医指导库', 'icon' => 'file-text-o', 'url' => \yii\helpers\Url::to(['article/zindex?ArticleSearchModel[type]=1']),
+                'items' => [
+                    ['label' => '指导文章列表', 'url' => ['article/zindex?ArticleSearchModel[type]=1']],
+                    ['label' => '宣教任务（设置自动）', 'url' => ['/article-send']],
+                ]
+            ];
+            $e = ['label' => '同步已签约数据', 'url' => ['/synchronization']];
         }
         ?>
 
@@ -38,11 +43,7 @@
                     $a,
                     $b,
                     ['label' => '社区数据统计', 'icon' => 'hospital-o', 'url' => \yii\helpers\Url::to(['user-doctor/index']),],
-                    ['label' => '中医指导库', 'icon' => 'file-text-o', 'url' => \yii\helpers\Url::to(['article/zindex?ArticleSearchModel[type]=1']),
-                        'items' => [
-$d,                            ['label' => '宣教任务（设置自动）', 'url' => ['/article-send']],
-                        ]
-                    ],
+                    $d,
                     ['label' => '健康知识库', 'icon' => 'file-text-o', 'url' => "#",
                         'items' => [
                             ['label' => '文章列表', 'url' => ['article/index']],
@@ -56,7 +57,7 @@ $d,                            ['label' => '宣教任务（设置自动）', 'ur
                             ['label' => '通知列表', 'url' => ['article/tindex?ArticleSearchModel[type]=2']],
                             ['label' => '发布通知', 'url' => ['article/tongzhi']],
                             ['label' => '平台召回用户统计', 'url' => ['/push-log']],
-$e,$c,                            ['label' => '疫情调查表下载', 'url' => ['/yiqing/down']],
+                            $e, $c, ['label' => '疫情调查表下载', 'url' => ['/yiqing/down']],
 
                         ]
                     ],
