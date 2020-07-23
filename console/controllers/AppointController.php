96075d309edf03d21fa9d140b1fae2a380640242
xiaohaoyong <xiaohaoyong@126.com>
2020-07-23 17:47:15 +0800

    大红门木材厂线下叫号机接口

--- console/controllers/AppointController.php
+++ console/controllers/AppointController.php
@@ -41,12 +41,12 @@ class AppointController extends Controller
                         'first' => array('value' => "宝宝家长您好",),
                         'keyword1' => ARRAY('value' => date('Y年m月d'),),
                         'keyword2' => ARRAY('value' => '您预约了' . date('Y年m月d', $day) . ' ' . Appoint::$typeText[$v->type] . '，请按照预约时间到达社区'),
-                        'remark' => ARRAY('value' => "鉴于目前北京市二级防控阶段，请点击此通知填写流行病学调查表，根据不同社区工作安排可能需要您出示调查结果，调查结果可以在公众号底部菜单我的->流行病学调查表中查看", 'color' => '#221d95'),
+                        'remark' => ARRAY('value' => "点击此处填写流行病学调查表，目前北京应急响应级别调整为三级，请酌情填写流行病学调查表，根据不同社区工作安排可能需要您出示调查结果，调查结果可以在公众号底部菜单我的->流行病学调查表中查看", 'color' => '#221d95'),
                     ];
                     if(in_array($v->doctorid,[192821,206260,257888,184793,160226])){
-                        $rs = WechatSendTmp::send($data, $openid, $temp, 'http://web.child.wedoctors.com.cn/question-naire/form?id=1&doctorid=' . $v->doctorid);
+                        $rs = WechatSendTmp::send($data, $openid, $temp, 'http://web.child.wedoctors.com.cn/question-naire/form?id=2  &doctorid=' . $v->doctorid);
                     }else{
-                        $data['remark']='鉴于目前北京市二级防控阶段，请核实您预约社区是否停诊,为了您的健康，请您出门带好口罩做好预防措施';
+                        $data['remark']='此消息为系统自动推送，如已取消请忽略。';
                         $rs = WechatSendTmp::send($data, $openid, $temp, '',['appid' => \Yii::$app->params['wxXAppId'], 'pagepath' => 'pages/appoint/view?id='.$v->id,]);
                     }
                     $log->addLog($rs ? 'true' : 'false');
