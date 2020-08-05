9b4cba7ca9a8534e62adf25975ad4e7c02ae2d49
xiaohaoyong <xiaohaoyong@126.com>
2020-08-05 09:20:01 +0800

    区后台管辖数

--- frontend/views/qappoint/list.php
+++ frontend/views/qappoint/list.php
@@ -18,18 +18,18 @@ $this->title="成人疫苗接种及两癌筛查预约"
                     <?=\yii\helpers\Html::a('在线预约',['wappoint/index'],['class'=>'button'])?>
                 </div>
             </div>
-<!--            <div class="item" style="padding: 10px; border-radius: 20px; border: 1px solid #999999;">-->
-<!--                <div class="item-content">-->
-<!--                    <div class="hospital">-->
-<!--                        <div class="name">预约两癌筛查</div>-->
-<!--                        <div class="address">温馨提示：筛查人群为北京市户籍年满35-64岁妇女，三年筛查一次（如2019年已筛查，下次筛查时间为2022年），必须携带身份证。注：请务必按照自己的预约时间段前来筛查现场，如有疑问请咨询预约社区或联系在线客服</div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="item-button">-->
-<!--                    <div class="phone"></div>-->
-<!---->
-<!--                </div>-->
-<!--            </div>-->
+            <div class="item" style="padding: 10px; border-radius: 20px; border: 1px solid #999999;">
+                <div class="item-content">
+                    <div class="hospital">
+                        <div class="name">预约两癌筛查</div>
+                        <div class="address">温馨提示：筛查人群为北京市户籍年满35-64岁妇女，三年筛查一次（如2019年已筛查，下次筛查时间为2022年），必须携带身份证。注：请务必按照自己的预约时间段前来筛查现场，如有疑问请咨询预约社区或联系在线客服</div>
+                    </div>
+                </div>
+                <div class="item-button">
+                    <div class="phone"></div>
+
+                </div>
+            </div>
         </div>
 
     </div>
