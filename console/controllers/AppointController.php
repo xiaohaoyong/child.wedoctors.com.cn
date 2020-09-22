1fdbbc2914527b192c6b849f7dd117b391043d67
xiaohaoyong <xiaohaoyong@126.com>
2020-09-22 16:44:46 +0800

    国医 彩虹本 数据导入

--- console/controllers/AppointController.php
+++ console/controllers/AppointController.php
@@ -9,33 +9,40 @@
 namespace console\controllers;
 
 
+use common\components\HttpRequest;
 use common\components\Log;
 use common\helpers\WechatSendTmp;
 use common\models\Appoint;
+use common\models\AppointOrder1;
+use common\models\HospitalForm;
+use common\models\UserDoctor;
 use common\models\UserLogin;
 use yii\base\Controller;
 
 class AppointController extends Controller
 {
-    public function actionOverdue(){
+    public function actionOverdue()
+    {
 
-        $day=strtotime(date('Y-m-d'));
-        Appoint::updateAll(['state' => 4], 'state=1 and appoint_date <='.$day);
+        $day = strtotime(date('Y-m-d'));
+        Appoint::updateAll(['state' => 4], 'state=1 and appoint_date <=' . $day);
 
     }
-    public function actionNotice(){
-        $day=strtotime('+1 day',strtotime(date('Y-m-d 00:00:00')));
-        $appoint=Appoint::find()->where(['appoint_date'=>$day])->andWhere(['not in','doctorid',[221895]])->andWhere(['!=','state',3])->all();
 
-        $log=new Log('appoint_notice');
-        if($appoint) {
+    public function actionNotice()
+    {
+        $day = strtotime('+1 day', strtotime(date('Y-m-d 00:00:00')));
+        $appoint = Appoint::find()->where(['appoint_date' => $day])->andWhere(['not in', 'doctorid', [221895]])->andWhere(['!=', 'state', 3])->all();
+
+        $log = new Log('appoint_notice');
+        if ($appoint) {
             foreach ($appoint as $k => $v) {
                 $log->addLog($v->id);
                 $log->addLog($v->userid);
-                $openid=UserLogin::getOpenid($v->userid);
+                $openid = UserLogin::getOpenid($v->userid);
                 $log->addLog($openid);
 
-                if($openid) {
+                if ($openid) {
                     $temp = 'AisY28B8z8_UDjX7xi6pay7Hh6kw420rAQwc6I1BBtE';
                     $data = [
                         'first' => array('value' => "宝宝家长您好",),
@@ -43,15 +50,90 @@ class AppointController extends Controller
                         'keyword2' => ARRAY('value' => '您预约了' . date('Y年m月d', $day) . ' ' . Appoint::$typeText[$v->type] . '，请按照预约时间到达社区'),
                         'remark' => ARRAY('value' => "点击此处填写流行病学调查表，目前北京应急响应级别调整为三级，请酌情填写流行病学调查表，根据不同社区工作安排可能需要您出示调查结果，调查结果可以在公众号底部菜单我的->流行病学调查表中查看", 'color' => '#221d95'),
                     ];
-                    if(in_array($v->doctorid,[192821,206260,257888,184793,160226])){
+                    if (in_array($v->doctorid, [192821, 206260, 257888, 184793, 160226])) {
                         $rs = WechatSendTmp::send($data, $openid, $temp, 'http://web.child.wedoctors.com.cn/question-naire/form?id=2  &doctorid=' . $v->doctorid);
-                    }else{
-                        $data['remark']='此消息为系统自动推送，如已取消请忽略。';
-                        $rs = WechatSendTmp::send($data, $openid, $temp, '',['appid' => \Yii::$app->params['wxXAppId'], 'pagepath' => 'pages/appoint/view?id='.$v->id,]);
+                    } else {
+                        $data['remark'] = '此消息为系统自动推送，如已取消请忽略。';
+                        $rs = WechatSendTmp::send($data, $openid, $temp, '', ['appid' => \Yii::$app->params['wxXAppId'], 'pagepath' => 'pages/appoint/view?id=' . $v->id,]);
                     }
                     $log->addLog($rs ? 'true' : 'false');
                     $log->saveLog();
                 }
+            }
+        }
+    }
+
+    //国医数据导入
+    public function actionGPush()
+    {
+        $date = date('Y-m-d');
+
+        // echo strtotime($date);exit;
+        $path = "https://rainbow.arxanfintech.com/book/rainbow/v1/query_hospital_reserve?reserve_datetime={$date}&access_token=0e35fa1c06da4b2e89e2f21860093bd7";
+        $curl = new HttpRequest($path, true, 10);
+        $userJson = $curl->get();
+        $list = json_decode($userJson, true);
+
+        $time = ['08' => 1, '09' => 2, '10' => 3, '13' => 4, '14' => 5, '15' => 6, '16' => 6];
+        foreach ($list['retbody'] as $k => $v) {
+            $appoint = [];
+
+            $rs = $v;
+            $birthday = strtotime($rs['baby_birthday']);
+            $appointOrder = AppointOrder1::findOne(['birthday' => $birthday, 'name' => $rs['baby_name']]);
+            if (!$appointOrder) {
+                $appointOrder = new AppointOrder1();
+                $appointOrder->birthday = $birthday;
+                $appointOrder->name = $rs['baby_name'];
+                $appointOrder->save();
+                var_dump($appointOrder->firstErrors);
+            }
+            $orderid = $appointOrder->id;
+
+            $ti = explode(':', $rs['reserve_period']);
+            $app = Appoint::find()->where(['appoint_time' => $time[$ti[0]], 'appoint_date' => strtotime($rs['reserve_datetime']), 'userid' => 0, 'doctorid' => 216593, 'type' => 2, 'orderid' => $orderid])->one();
+            $app = $app ? $app : new Appoint();
+            $appoint['appoint_time'] = $time[$ti[0]];
+            $appoint['appoint_date'] = strtotime($rs['reserve_datetime']);
+            $appoint['userid'] = 0;
+            $appoint['doctorid'] = 216593;
+            $appoint['type'] = 2;
+            $appoint['childid'] = 0;
+            $appoint['phone'] = 0;
+            $appoint['state'] = 2;
+            $appoint['login'] = 0;
+            $appoint['mode'] = 2;
+            $appoint['vaccine'] = 0;
+            $appoint['month'] = 0;
+            $appoint['orderid'] = $orderid;
+            $app->load(['Appoint' => $appoint]);
+            $app->save();
+            var_dump($app->firstErrors);
+        }
+
+
+        $satime = strtotime('-30 day', strtotime(date('Y-m-d')));
+        $doctors = UserDoctor::find()->where(['userid' => 216593])->all();
+        foreach ($doctors as $k => $v) {
+            echo $v->name;
+            for ($stime = $satime; $stime < time(); $stime = strtotime('+1 day', $stime)) {
+                echo date('Ymd', $stime);
+                $etime = strtotime('+1 day', $stime);
+
+                $appoint = Appoint::find()->where(['doctorid' => $v->userid])
+                    ->andWhere(['appoint_date' => $stime])
+                    ->andWhere(['!=', 'state', 3])
+                    ->count();
+                $rs = [];
+
+                $rs['appoint_num'] = $appoint;
+                $rs['doctorid'] = $v->userid;
+                $rs['date'] = $stime;
+                $hospitalFrom = HospitalForm::find()->where(['doctorid' => $v->userid])->andWhere(['date' => $stime])->one();
+                $hospitalFrom = $hospitalFrom ? $hospitalFrom : new HospitalForm();
+                $hospitalFrom->load(['HospitalForm' => $rs]);
+                $hospitalFrom->save();
+                echo "\n";
             }
         }
     }
