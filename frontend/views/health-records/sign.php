e831eef8ab5a0d55e50297b2810323f387c93de2
xiaohaoyong <xiaohaoyong@126.com>
2020-11-05 13:42:33 +0800

    成人疫苗号满提示修改

--- frontend/views/health-records/sign.php
+++ frontend/views/health-records/sign.php
@@ -11,30 +11,35 @@ frontend\assets\SignAsset::register($this);
         body {
             background: #F3F3F3;
         }
+        #signature{width:100vw;height: 100vh;}
     </style>
-<div class="health-records-sign">
-    <div id="signature" style="padding: 10px 10px;"></div>
-    <div class="right-button">
-        <div class="health-records-button">
-            <button id="save">确认</button>
+    <div class="health-records-sign">
+        <div id="signature" style="padding: 10px 10px;"></div>
+        <div class="right-button">
+            <div class="health-records-button">
+                <button id="save">确认</button>
+            </div>
+            <div class="health-records-txt">请于白色区域签字确认</div>
         </div>
-        <div class="health-records-txt">请于白色区域签字确认</div>
+        <div class="left-title">
+            签字既代表阅读并接受协议，<a href="/img/xieyi.pdf" target="_blank"> 查看协议模板</a>
+        </div>
     </div>
-    <div class="left-title">
-        签字既代表阅读并接受协议，<a href="/img/xieyi.pdf" target="_blank"> 查看协议模板</a>
-    </div>
-</div>
 <?php
 $updateJs = <<<JS
     var width=$(window).width()-20;
     var height=$(window).height()-25;
     $(document).ready(function() {
-        $("#signature").jSignature({
-                'width':width ,
-                'height': height,
-                'background-color':'#fff'
-        })
+        
+        $("#signature").jSignature('init',{height:'100%',width:'100%','background-color':'#fff'})
     })
+        //监听屏幕旋转
+    $(window).on('orientationchange', function() {
+        var width=$(window).width()-20;
+        var height=$(window).height()-25;
+       
+$("#signature").jSignature('clear');
+    });
     $('#save').click(function () {
             if( $("#signature").jSignature('getData', 'native').length == 0){
                 alert("请先进行签名");
