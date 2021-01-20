c78c9f55a0fb94c8618cc11d331130cafbcf6d8c
xiaohaoyong <xiaohaoyong@126.com>
2021-01-20 14:53:35 +0800

    流调增加签字功能

--- console/controllers/DataController.php
+++ console/controllers/DataController.php
@@ -88,9 +88,8 @@ class DataController extends \yii\console\Controller
                 'keyword2' => ARRAY('value' => '2021年01月20日 19点30分'),
                 'remark' => ARRAY('value' => ""),
             ];
-            $rs = WechatSendTmp::send($data, 'o5ODa0451fMb_sJ1D1T4YhYXDOcg', 'NNm7CTQLIY66w3h4FzSrp_Lz54tA12eFgds07LRMQ8g', 'https://appsx0v9q8i8331.h5.xiaoeknow.com/v1/course/alive/l_60065927e4b0ab9a2549d642?type=2');
+            $rs = WechatSendTmp::send($data, $v, 'NNm7CTQLIY66w3h4FzSrp_Lz54tA12eFgds07LRMQ8g', 'https://appsx0v9q8i8331.h5.xiaoeknow.com/v1/course/alive/l_60065927e4b0ab9a2549d642?type=2');
             sleep(1);
-            exit;
         }
         var_dump($login);
         exit;
