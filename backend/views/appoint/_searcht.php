62de4a05d215605c4f9ed25c4080bfea6d6bd487
wangzhen <xiaohaoyong@126.com>
2021-02-02 09:45:51 +0800

    更新

--- backend/views/appoint/_searcht.php
+++ backend/views/appoint/_searcht.php
@@ -29,10 +29,18 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'doctorid')->dropDownList(\common\models\UserDoctor::find()->select('name')->indexBy('userid')->where(['county' => $model['county']])->column(), ['prompt' => '请选择']) ?>
 
 
-    <?= $form->field($model, 'appoint_dates')->widget(\kartik\date\DatePicker::className(),['pluginOptions' => [
+    <?= $form->field($model, 'appoint_dates')->widget(\kartik\date\DatePicker::className(), ['pluginOptions' => [
         'format' => 'yyyy-mm-dd',
+        'autocomplete'=>'off',
         'todayHighlight' => true
-    ]])?>
+    ]]) ?>
+    <?= $form->field($model, 'appoint_dates_end')->widget(\kartik\date\DatePicker::className(), ['pluginOptions' => [
+        'format' => 'yyyy-mm-dd',
+        'autocomplete'=>'off',
+        'todayHighlight' => true
+    ]]) ?>
+    <?php echo $form->field($model, 'vaccine')->dropDownList(\common\models\Vaccine::find()->select('name')->indexBy('id')->column(), ['prompt' => '请选择']) ?>
+
 
     <?php  echo $form->field($model, 'appoint_time')->dropDownList(\common\models\Appoint::$timeText,['prompt'=>'请选择']) ?>
 
