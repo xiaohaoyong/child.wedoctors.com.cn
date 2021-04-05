0a56fae1838489e3bb9d899a7e10130dfbddbc9f
xiaohaoyong <xiaohaoyong@126.com>
2021-04-05 23:15:01 +0800

    核酸检测

--- frontend/views/health-records/form2.php
+++ frontend/views/health-records/form2.php
@@ -9,7 +9,9 @@ use common\models\HealthRecords;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 <style>
-    body{background: #ffffff;}
+    body {
+        background: #ffffff;
+    }
 </style>
 <div class="health-records">
     <div class="header">
@@ -33,28 +35,43 @@ use common\models\HealthRecords;
 
     </div>
     <?php $form = ActiveForm::begin([
-            'options'=>[
-                'class'=>'form-horizontal',
-            ],
+        'options' => [
+            'class' => 'form-horizontal',
+        ],
         'fieldConfig' => [  //统一修改字段的模板
             'template' => "<div class='form-group-info'>{label}\n<div class='form-input'>{input}</div></div>{error}",
-            'inputOptions'=>['class'=>''],
+            'inputOptions' => ['class' => ''],
         ],
     ]);
     ?>
+    <div class="title">请选择服务包</div>
+    <div class="form-group ">
+        <div class="form-group-info" style="margin: 20px; display: flex;justify-content: space-between;">
+            <label for="healthrecords-field29">“家庭医生签约进校园”学龄儿童服务包</label>
+            <div>
+                <?=Html::checkbox('fuwb',true)?>
+            </div>
+        </div>
+        <div class="form-group-info" style="margin: 20px; display: flex;justify-content: space-between;">
+            <label for="healthrecords-field29">基本服务包</label>
+            <div>
+                <?=Html::checkbox('fuwb',true,['disabled'=>'true'])?>
+            </div>
+        </div>
+    </div>
     <div class="title">个人信息</div>
-    <?= $form->field($model, 'field29')->textInput(['maxlength' => true,'placeholder'=>'请填写学生姓名']) ?>
+    <?= $form->field($model, 'field29')->textInput(['maxlength' => true, 'placeholder' => '请填写学生姓名']) ?>
 
-    <?= $form->field($model, 'field30')->dropDownList(\common\models\HealthRecordsSchool::find()->select('name')->indexBy('id')->where(['doctorid'=>$doctorid])->column(),['prompt'=>'请选择']) ?>
-    <?= $form->field($model, 'field34')->textInput(['maxlength' => true,'placeholder'=>'请填写学生年级']) ?>
-    <?= $form->field($model, 'field40')->textInput(['maxlength' => true,'placeholder'=>'请填写学生班级']) ?>
-    <?= $form->field($model, 'field41')->textInput(['placeholder'=>'请填写'.$model->getAttributeLabel('field41')])?>
+    <?= $form->field($model, 'field30')->dropDownList(\common\models\HealthRecordsSchool::find()->select('name')->indexBy('id')->where(['doctorid' => $doctorid])->column(), ['prompt' => '请选择']) ?>
+    <?= $form->field($model, 'field34')->textInput(['maxlength' => true, 'placeholder' => '请填写学生年级']) ?>
+    <?= $form->field($model, 'field40')->textInput(['maxlength' => true, 'placeholder' => '请填写学生班级']) ?>
+    <?= $form->field($model, 'field41')->textInput(['placeholder' => '请填写' . $model->getAttributeLabel('field41')]) ?>
 
-    <?= $form->field($model, 'field42')->dropDownList(HealthRecords::$field41Txt,['prompt'=>'请选择'])?>
-    <?= $form->field($model, 'field3')->textInput(['maxlength' => true,'placeholder'=>'请填写家长姓名']) ?>
+    <?= $form->field($model, 'field42')->dropDownList(HealthRecords::$field41Txt, ['prompt' => '请选择']) ?>
+    <?= $form->field($model, 'field3')->textInput(['maxlength' => true, 'placeholder' => '请填写家长姓名']) ?>
 
-    <?= $form->field($model, 'field4')->textInput(['placeholder'=>'请填写'.$model->getAttributeLabel('field4')]) ?>
-    <?= $form->field($model, 'field6')->textInput(['placeholder'=>'请填写'.$model->getAttributeLabel('field6')]) ?>
+    <?= $form->field($model, 'field4')->textInput(['placeholder' => '请填写' . $model->getAttributeLabel('field4')]) ?>
+    <?= $form->field($model, 'field6')->textInput(['placeholder' => '请填写' . $model->getAttributeLabel('field6')]) ?>
 
     <div style="margin-left: 20px; margin-top: 10px;">同意签约点击提交</div>
     <div class="form-group button">
