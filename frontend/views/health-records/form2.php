70d57b887acd9fd86a16a15a72d66e89335c8372
xiaohaoyong <xiaohaoyong@126.com>
2021-04-07 00:36:03 +0800

    23

--- frontend/views/health-records/form2.php
+++ frontend/views/health-records/form2.php
@@ -52,7 +52,7 @@ use yii\bootstrap\Modal;
                 <?=Html::checkbox('fuwb',true)?>
                 <?php
                 echo Html::a('详情', '#', [
-                    'data-target' => '#modal' ,//关联模拟框(模拟框的ID)
+                    'data-target' => '#jt-modal' ,//关联模拟框(模拟框的ID)
                     'data-toggle' => "modal", //定义为模拟框 触发按钮
                     'data-id' => 'jt',
                 ]);
@@ -60,23 +60,119 @@ use yii\bootstrap\Modal;
             </div>
         </div>
         <?php
-        Modal::begin([
+        \yii\bootstrap\Modal::begin([
             'class' => 'modal',
-            'id' => 'jt',
+            'id' => 'jt-modal',
             'header' => '<h5>家庭医生签约进校园”学龄儿童服务包</h5>',
         ]);
         ?>
-       1123123
+        <div class="title">学龄儿童服务包</div>
+        <div class="form-group ">
+            <div class="info-title">基础项目</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">传染病疫情防控指导</div>
+            <div class="form-table-td2">通过对学校、教职工培训指导，开展传染病疫情防控指导、技术支持等服务</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">儿童常见健康问题指导</div>
+            <div class="form-table-td2">针对儿童心理、肥胖、口腔和近视等常见健康问题，通过微信小程序给予相关健康知识推送指导</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">龋齿预防</div>
+            <div class="form-table-td2">一到三年级儿童（7-9岁）每年一次窝沟封闭，同时进行龋齿检查、防龋指导</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">预防接种</div>
+            <div class="form-table-td2">疫苗接种提醒、咨询、流感疫苗接种指导以及计划内免疫接种</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">中医外治法防治青少年近视（自愿选择，非强制）</div>
+            <div class="form-table-td2">中医按摩、点穴、拔罐、耳穴压豆、梅花针等方法防治青少年近视（收费，按照医保收费标准执行。自愿选择。）</div>
+            <div class="form-table-td3"><?=Html::checkbox('HealthRecords[field38]',false)?></div>
+        </div>
 
         <?php
-        Modal::end();
+        \yii\bootstrap\Modal::end();
         ?>
         <div class="form-group-info" style="margin: 20px; display: flex;justify-content: space-between;">
             <label for="healthrecords-field29">基本服务包</label>
             <div>
                 <?=Html::checkbox('fuwb',true,['disabled'=>'true'])?>
+                <?php
+                echo Html::a('详情', '#', [
+                    'data-target' => '#jc-modal' ,//关联模拟框(模拟框的ID)
+                    'data-toggle' => "modal", //定义为模拟框 触发按钮
+                    'data-id' => 'jc',
+                ]);
+                ?>
             </div>
         </div>
+        <?php
+        \yii\bootstrap\Modal::begin([
+            'class' => 'modal',
+            'id' => 'jc-modal',
+            'header' => '<h5>基本服务包</h5>',
+        ]);
+        ?>
+        <div class="form-group ">
+            <div class="info-title" style="text-align: center">基本医疗服务项目</div>
+        </div>
+        <div class="form-table">
+            <div class="form-table-td1">全科预约诊疗</div>
+            <div class="form-table-td2">全科门诊就诊时，为签约居民提供专人管理、专人诊疗、预约优先就诊</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">预约转诊</div>
+            <div class="form-table-td2">根据病情需要，为签约患者提供上级大医院预约挂号及转诊等服务</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">用药指导</div>
+            <div class="form-table-td2">为签约居民提供就医、用药指导服务</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-group ">
+            <div class="info-title">基本公共卫生及健康管理服务项目</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">康复管理</div>
+            <div class="form-table-td2">为符合条件的签约居民提供康复服务</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">签约“四个一”服务</div>
+            <div class="form-table-td2">建立1份健康档案，签订1份家医签约服务协议，发放1个联系卡/表/手册，定期推送1条短信/微信</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">健康咨询服务</div>
+            <div class="form-table-td2">为签约居民提供电话健康咨询服务</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">免费健康信息送达</div>
+            <div class="form-table-td2">利用微信、短信、APP等信息化手段，定期发送随访提醒服务或个性化健康教育等信息</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+        <div class="form-group ">
+            <div class="info-title">特色服务项目</div>
+        </div>
+        <div class="form-table ">
+            <div class="form-table-td1">朝阳区家医服务APP</div>
+            <div class="form-table-td2">登录朝阳区家医服务APP，享受查询、互动，获得科学、权威健康资讯</div>
+            <div class="form-table-td3">免费</div>
+        </div>
+
+
+        <?php
+        \yii\bootstrap\Modal::end();
+        ?>
     </div>
     <div class="title">个人信息</div>
     <?= $form->field($model, 'field29')->textInput(['maxlength' => true, 'placeholder' => '请填写学生姓名']) ?>
