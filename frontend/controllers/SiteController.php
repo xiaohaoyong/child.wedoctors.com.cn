2a8800fa01e287ea49af48904523c74cf6da185f
wangzhen <xiaohaoyong@126.com>
2021-04-09 21:58:11 +0800

    123

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -75,82 +75,102 @@ class SiteController extends Controller
         $merge->title="本次使用的是哪种新冠疫苗？";
         $merge->content=1;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="新冠疫苗打几针？间隔时间？";
         $merge->content=2;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="新冠疫苗有必要接种吗？";
         $merge->content=3;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="新冠疫苗的安全性和有效性如何？";
         $merge->content=4;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="接种新冠疫苗怎么预约？";
         $merge->content=5;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="接种新冠疫苗可能会出现什么不良反应？出现不良反应怎么办？";
         $merge->content=6;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="60岁以上和18岁以下人群可以接种新冠疫苗吗？";
         $merge->content=7;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="哪些人不适宜接种新冠疫苗？";
         $merge->content=8;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="哪些人不适宜打第二剂次疫苗？";
         $merge->content="9";
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="接种新冠疫苗前需要做哪些准备？";
         $merge->content=10;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="接种新冠疫苗时应注意什么？";
         $merge->content=11;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="接种新冠疫苗后还要做什么？";
         $merge->content=12;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="接种完新冠疫苗是否可以喝酒、吃辛辣食物？";
         $merge->content=13;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="接种完新冠疫苗是否可以洗澡？";
         $merge->content=14;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="接种新冠疫苗后可以放松个人防护措施吗？";
         $merge->content=15;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="慢性病患者可以接种新冠疫苗吗？（常年吃药患有高血压的人、糖尿病、甲状腺、心脏病等）";
         $merge->content=16;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="有过敏史的可以接种新冠疫苗吗？";
         $merge->content=17;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="备孕期可以打新冠疫苗吗？接种后多久能要孩子？";
         $merge->content=18;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="新冠肺炎治愈人群需要接种新冠疫苗吗？";
         $merge->content=19;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="外籍人士可以在北京打新冠疫苗吗？";
         $merge->content=20;
         $merge->save();
+        $merge = new Merge();
 
         $merge->title="接种新冠疫苗花钱吗？";
         $merge->content=21;
