6cfcb070c5cc973b6483bd42d492dba20679560b
xiaohaoyong <xiaohaoyong@126.com>
2021-04-15 17:29:06 +0800

    核酸检测

--- frontend/views/health-records/form2.php
+++ frontend/views/health-records/form2.php
@@ -102,8 +102,7 @@ use yii\bootstrap\Modal;
         ?>
         <div class="form-group-info" style="margin: 20px;">
             <div>
-                <?= $form->field($model, 'field43')->checkbox(['disabled'=>'true','checkend'=>true]) ?>
-
+                <?=Html::activeCheckbox($model,'field43',['disabled'=>'true','checkend'=>true])?>
                 <?php
                 echo Html::a('详情', '#', [
                     'data-target' => '#jc-modal' ,//关联模拟框(模拟框的ID)
