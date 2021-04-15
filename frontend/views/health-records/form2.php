bd55da7dec202aa8e1975744f3e54870dca18c28
xiaohaoyong <xiaohaoyong@126.com>
2021-04-15 17:04:59 +0800

    核酸检测

--- frontend/views/health-records/form2.php
+++ frontend/views/health-records/form2.php
@@ -103,7 +103,7 @@ use yii\bootstrap\Modal;
         <div class="form-group-info" style="margin: 20px; display: flex;justify-content: space-between;">
             <label for="healthrecords-field29">基本服务包</label>
             <div>
-                <?= $form->field($model, 'field43')->checkbox(['disabled'=>'true','checkend'=>true])->label(false) ?>
+                <?= $form->field($model, 'field43')->checkbox(['disabled'=>'true','checkend'=>true])->label(null) ?>
 
                 <?php
                 echo Html::a('详情', '#', [
