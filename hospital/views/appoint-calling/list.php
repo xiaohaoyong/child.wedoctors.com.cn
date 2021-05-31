d22762d82382957efc18dbc6f707be726ed97741
xiaohaoyong <xiaohaoyong@126.com>
2021-05-31 13:51:03 +0800

    核酸检测

--- hospital/views/appoint-calling/list.php
+++ hospital/views/appoint-calling/list.php
@@ -9,12 +9,7 @@
  <div data="汪振请到第一诊室"></div>
  <div data="CCC请到第一诊室"></div>
  <div data="BBBB请到第一诊室"></div>
-
- <audio id="myAudio" controls>
-     <source src="" id="myAu" type="audio/mpeg">
-     您的浏览器不支持 audio 元素。
- </audio>
-
+<div id="test"></div>
  <button id="kaiqi" onclick="f()">开启</button>
 <script>
     function f() {
@@ -27,9 +22,11 @@
 
  $updateJs = <<<JS
    $.get('http://hospital.child.wedoctors.com.cn/appoint-calling/ttl?text=是点击老师点击',function (e) {
-       $('#myAu').attr('src',e.src);
+       console.log(e.src);
+       var audio=' <audio id="myAudio" controls><source src="'+e.src+'" id="myAu" type="audio/mpeg">您的浏览器不支持 audio 元素。</audio>';
+       $('#test').html(audio);
        vMP3 = document.getElementById("myAudio");
-        vMP3.play();
+       vMP3.play();
     })
 JS;
  $this->registerJs($updateJs);
