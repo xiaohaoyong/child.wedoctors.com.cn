8e64e7fe86fa3c6666c391c96e8d11ee68371029
xiaohaoyong <xiaohaoyong@126.com>
2021-05-31 14:30:36 +0800

    核酸检测

--- hospital/views/appoint-calling/list.php
+++ hospital/views/appoint-calling/list.php
@@ -6,10 +6,10 @@
  * Time: 下午12:06
  */
 ?>
- <div data="汪振请到第一诊室"></div>
- <div data="CCC请到第一诊室"></div>
- <div data="BBBB请到第一诊室"></div>
-<div id="test"></div>
+ <div class="item" data-level="0" data="汪振请到第一诊室"></div>
+ <div class="item" data-level="0" data="CCC请到第一诊室"></div>
+ <div class="item" data-level="0" data="BBBB请到第一诊室"></div>
+<div id="test" style="display: none;"></div>
  <button id="kaiqi" onclick="f()">开启</button>
 <script>
     function f() {
@@ -21,13 +21,22 @@
 
 
  $updateJs = <<<JS
-   $.get('http://hospital.child.wedoctors.com.cn/appoint-calling/ttl?text=是点击老师点击',function (e) {
-       console.log(e.src);
-       var audio=' <audio id="myAudio" controls><source src="'+e.src+'" id="myAu" type="audio/mpeg">您的浏览器不支持 audio 元素。</audio>';
-       $('#test').html(audio);
-       vMP3 = document.getElementById("myAudio");
-       vMP3.play();
-    })
+ $(".item").each(function(){
+     var level=$(this).attr('data-level');
+     if(level==1){
+              var text=$(this).attr('data');
+
+     
+        $.get('http://hospital.child.wedoctors.com.cn/appoint-calling/ttl?text='+text,function (e) {
+           console.log(e.src);
+           var audio=' <audio id="myAudio" controls><source src="'+e.src+'" id="myAu" type="audio/mpeg">您的浏览器不支持 audio 元素。</audio>';
+           $('#test').html(audio);
+           vMP3 = document.getElementById("myAudio");
+           vMP3.play();
+        })
+    }
+  });
+   
 JS;
  $this->registerJs($updateJs);
 
