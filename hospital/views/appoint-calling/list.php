f3bff96ceb2d02686899a3f43d77940d0f770a75
xiaohaoyong <xiaohaoyong@126.com>
2021-05-31 13:34:34 +0800

    核酸检测

--- hospital/views/appoint-calling/list.php
+++ hospital/views/appoint-calling/list.php
@@ -10,13 +10,17 @@
  <div data="CCC请到第一诊室"></div>
  <div data="BBBB请到第一诊室"></div>
 
- <audio id="myAudio" controls style="display: none">
+ <audio id="myAudio" controls>
      <source src="" id="myAu" type="audio/mpeg">
      您的浏览器不支持 audio 元素。
  </audio>
 
  <button id="kaiqi" onclick="f()">开启</button>
 <script>
+    function f() {
+        vMP3 = document.getElementById("myAudio");
+        vMP3.play();
+    }
 </script>
  <?php
 
