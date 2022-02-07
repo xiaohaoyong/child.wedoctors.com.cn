1466823a35dbb915b343195eef07ae155ba6a694
xiaohaoyong@126.com <xiaohaoyong@126.com>
2022-02-07 10:20:32 +0800

    核酸检测

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -98,7 +98,8 @@ class UserController extends Controller
             $path = "/sns/jscode2session?appid=" . \Yii::$app->params['wxXAppId'] . "&secret=" . \Yii::$app->params['wxXAppSecret'] . "&js_code=" . $code . "&grant_type=authorization_code";
             $curl = new HttpRequest(\Yii::$app->params['wxUrl'] . $path, true, 10);
             $userJson = $curl->get();
-            var_dump($curl->getMsg());exit;
+            var_dump($curl->getMsg());
+            var_dump($curl->getCode());exit;
             $log->addLog("wxrequist:".$userJson);
             $user = json_decode($userJson, true);
             //$log->addLog($user);
