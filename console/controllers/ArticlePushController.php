7fd7f540d8e071f5cf594046a3a96bcf563b3210
wangzhen <xiaohaoyong@126.com>
2022-03-30 11:00:07 +0800

    324

--- console/controllers/ArticlePushController.php
+++ console/controllers/ArticlePushController.php
@@ -1,5 +1,6 @@
 <?php
 namespace console\controllers;
+use common\helpers\WechatSendTmp;
 use common\models\ArticlePushVaccine;
 use common\models\Notice;
 use yii\console\Controller;
@@ -18,39 +19,40 @@ class ArticlePushController extends Controller
         $todayE=strtotime('+1 day',$todayS);
         switch ($type){
             case 1:
-                $openids=\common\models\WeOpenid::find()->where(['>=','createtime',$todayS])
-                    ->select('openid')
-                    ->andWhere(['<','createtime',$todayE])
-                    ->groupBy('openid')
-                    ->column();
-                $aid=1371;
-                $title='新上线宝妈';
-                $first='您好，在您接种疫苗前，请认真阅读疫苗接种前后注意事项，提前做好相应功课。';
-                break;
-            case 2:
-                $preg=\common\models\Pregnancy::find()->where(['>','field11',strtotime('-28 week')])
-                    ->select('familyid')
-                    ->groupBy('familyid')
-                    ->column();
-                $openids=\common\models\UserLogin::find()->select('openid')
-                    ->where(['in','userid',$preg])->andWhere(['!=','openid',''])->column();
-                $aid=1369;
-                $title='孕晚期准妈';
-                $first='准妈您好，在您的宝宝出生时需要接种的疫苗，请认真阅读疫苗接种前注意事项，喜迎宝宝出生。';
-                break;
-            case 3:
-                $childs=\common\models\ChildInfo::find()->where(['<','birthday',strtotime('-1 month')])
-                    ->andWhere(['>','birthday',strtotime('-3 month')])
+                $childs=\common\models\ChildInfo::find()->where(['<','birthday',strtotime('-60 day')])
+                    ->andWhere(['>','birthday',strtotime('-74 day')])
                     ->select('userid')
                     ->groupBy('userid')
                     ->column();
                 $openids=\common\models\UserLogin::find()->select('openid')
                     ->where(['in','userid',$childs])->andWhere(['!=','openid',''])->column();
-                $aid=1370;
-                $title='一，二月龄宝宝家长';
+                $aid=1979;
+                $title='二至三月龄宝宝家长';
                 $first='宝妈您好，您的宝宝已经到达了接种脊灰疫苗的月龄，请认真阅读脊灰疫苗接种前注意事项，选择自己宝宝适合的接种方式。';
                 break;
-
+            case 2:
+                $preg=\common\models\Pregnancy::find()->where(['>','field11',strtotime('-36 week')])
+                    ->select('familyid')
+                    ->groupBy('familyid')
+                    ->column();
+                $openids=\common\models\UserLogin::find()->select('openid')
+                    ->where(['in','userid',$preg])->andWhere(['!=','openid',''])->column();
+                $aid=1979;
+                $title='孕晚期准妈';
+                $first='准妈您好，在您的宝宝出生时需要接种的疫苗，请认真阅读疫苗接种前注意事项，喜迎宝宝出生。';
+                break;
+            case 3:
+                $childs=\common\models\ChildInfo::find()->where(['<','birthday',strtotime('-30 day')])
+                    ->andWhere(['>','birthday',strtotime('-44 day')])
+                    ->select('userid')
+                    ->groupBy('userid')
+                    ->column();
+                $openids=\common\models\UserLogin::find()->select('openid')
+                    ->where(['in','userid',$childs])->andWhere(['!=','openid',''])->column();
+                $aid=1979;
+                $title='一至二月龄宝宝家长';
+                $first='宝妈您好，您的宝宝已经到达了接种脊灰疫苗的月龄，请认真阅读脊灰疫苗接种前注意事项，选择自己宝宝适合的接种方式。';
+                break;
         }
 
         if($openids && $aid){
