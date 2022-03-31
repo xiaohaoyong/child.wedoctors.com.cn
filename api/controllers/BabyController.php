98538c2a5aacb2c0c1ad746c876b1c798514a2d8
xiaohaoyong@126.com <xiaohaoyong@126.com>
2022-04-01 01:56:27 +0800

    核酸检测

--- api/controllers/BabyController.php
+++ api/controllers/BabyController.php
@@ -18,77 +18,93 @@ use common\models\Vaccine;
 
 class BabyController extends Controller
 {
-    public function actionSex($period,$sex){
-        $tags=BabyToolTag::findOne($period);
-        if($sex==2){
-            $tags->mother_num=$tags->mother_num+1;
-        }elseif($sex==1){
-            $tags->father_num=$tags->father_num+1;
+    public function actionSex($period, $sex)
+    {
+        $tags = BabyToolTag::findOne($period);
+        if ($sex == 2) {
+            $tags->mother_num = $tags->mother_num + 1;
+        } elseif ($sex == 1) {
+            $tags->father_num = $tags->father_num + 1;
         }
         $tags->save();
 
     }
-    public function actionTag(){
 
-        $tags=BabyToolTag::find()->orderBy('week asc,id asc')->all();
+    public function actionTag()
+    {
+
+        $tags = BabyToolTag::find()->orderBy('week asc,id asc')->all();
         return $tags;
     }
-    public function actionListNew($period){
 
-        $list=BabyTool::findAll(['period'=>$period]);
-        $nlist=BabyGuide::findAll(['period'=>$period]);
-        $like=BabyToolLike::findOne(['bid'=>$period,'loginid'=>$this->userLogin->id,'type'=>1]);
-        $isCollection=BabyToolLike::findOne(['bid'=>$period,'loginid'=>$this->userLogin->id,'type'=>2]);
-        $likeCount=BabyToolLike::find()->where(['bid'=>$period,'type'=>1])->count();
+    public function actionListNew($period)
+    {
 
-        $visit=BabyToolLike::find()->where(['bid'=>$period,'type'=>3,'loginid'=>$this->userLogin->id])->count();
-        if(!$visit){
-            $visit=new BabyToolLike();
-            $visit->bid=$period;
-            $visit->userid=$this->userid;
-            $visit->loginid=$this->userLogin->id;
-            $visit->type=3;
+        $list = BabyTool::findAll(['period' => $period]);
+        $nlist = BabyGuide::findAll(['period' => $period]);
+        $like = BabyToolLike::findOne(['bid' => $period, 'loginid' => $this->userLogin->id, 'type' => 1]);
+        $isCollection = BabyToolLike::findOne(['bid' => $period, 'loginid' => $this->userLogin->id, 'type' => 2]);
+        $likeCount = BabyToolLike::find()->where(['bid' => $period, 'type' => 1])->count();
+
+        $visit = BabyToolLike::find()->where(['bid' => $period, 'type' => 3, 'loginid' => $this->userLogin->id])->count();
+        if (!$visit) {
+            $visit = new BabyToolLike();
+            $visit->bid = $period;
+            $visit->userid = $this->userid;
+            $visit->loginid = $this->userLogin->id;
+            $visit->type = 3;
             $visit->save();
         }
 
-        return ['list'=>$list,'nlist'=>$nlist,'isLike'=>$like?true:false,'likeCount'=>$likeCount,'isCollection'=>$isCollection?true:false];
+        return ['list' => $list, 'nlist' => $nlist, 'isLike' => $like ? true : false, 'likeCount' => $likeCount, 'isCollection' => $isCollection ? true : false];
     }
-    public function actionList($period){
-        $list=BabyTool::findAll(['period'=>$period]);
-        return $list;
-    }
-    public function actionVlist(){
 
-        $list=Vaccine::find()->where(['>','source',0])->orderBy('source asc')->all();
+    public function actionList($period)
+    {
+        $list = BabyTool::findAll(['period' => $period]);
         return $list;
     }
-    public function actionVview($id){
-        $view=Vaccine::findOne($id);
+
+    public function actionVlist()
+    {
+
+
+        $list = Vaccine::find()->where(['>', 'source', 0])->orderBy('source asc')->all();
+        return $list;
+    }
+
+    public function actionVview($id)
+    {
+        $view = Vaccine::findOne($id);
         return $view;
     }
-    public function actionLike($id,$type){
-        $like=BabyToolLike::findOne(['bid'=>$id,'loginid'=>$this->userLogin->id,'type'=>1]);
-        if($like && $type==2){
+
+    public function actionLike($id, $type)
+    {
+        $like = BabyToolLike::findOne(['bid' => $id, 'loginid' => $this->userLogin->id, 'type' => 1]);
+        if ($like && $type == 2) {
             $like->delete();
-        }elseif(!$like && $type==1){
-            $like=new BabyToolLike();
-            $like->bid=$id;
-            $like->userid=$this->userid;
-            $like->type=1;
-            $like->loginid=$this->userLogin->id;
+        } elseif (!$like && $type == 1) {
+            $like = new BabyToolLike();
+            $like->bid = $id;
+            $like->userid = $this->userid;
+            $like->type = 1;
+            $like->loginid = $this->userLogin->id;
             $like->save();
         }
     }
-    public function actionCollection($id,$type){
-        $like=BabyToolLike::findOne(['bid'=>$id,'loginid'=>$this->userLogin->id,'type'=>2]);
-        if($like && $type==2){
+
+    public function actionCollection($id, $type)
+    {
+        $like = BabyToolLike::findOne(['bid' => $id, 'loginid' => $this->userLogin->id, 'type' => 2]);
+        if ($like && $type == 2) {
             $like->delete();
-        }elseif(!$like && $type==1){
-            $like=new BabyToolLike();
-            $like->bid=$id;
-            $like->userid=$this->userid;
-            $like->type=2;
-            $like->loginid=$this->userLogin->id;
+        } elseif (!$like && $type == 1) {
+            $like = new BabyToolLike();
+            $like->bid = $id;
+            $like->userid = $this->userid;
+            $like->type = 2;
+            $like->loginid = $this->userLogin->id;
             $like->save();
         }
     }
---
+++ api/modules/v4/controllers/BabyController.php
@@ -0,0 +1,258 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangzhen
+ * Date: 2018/1/30
+ * Time: 上午11:51
+ */
+
+namespace api\modules\v4\controllers;
+
+use api\controllers\Controller;
+
+use common\models\BabyGuide;
+use common\models\BabyTool;
+use common\models\BabyToolLike;
+use common\models\BabyToolTag;
+use common\models\Vaccine;
+
+class BabyController extends \api\controllers\BabyController
+{
+
+    public function actionVlist()
+    {
+
+        $ages = [
+            [
+                'name' => '一月龄',
+                'vaccines' => [
+                    'left' => [
+                        [
+
+                            'url' => '/pages/tool/vview/index?id=3',
+                            'name' => '乙肝疫苗(第2剂)',
+                        ]
+                    ],
+                    'right' => [
+                    ],
+
+                ]
+            ],
+            [
+                'name' => '二月龄',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=4',
+                            'name' => '脊灰疫苗(IPV)(第1剂)',
+                        ],
+                    ],
+                    'right' => [
+                        [
+                            'url' => 1,
+                            'name' => '五联疫苗',
+                        ],
+                    ],
+
+                ]
+            ],
+            [
+                'name' => '三月龄',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=6',
+                            'name' => '无细胞百白破疫苗	',
+                        ],
+                        [
+                            'url' => '/pages/tool/vview/index?id=5',
+                            'name' => '脊灰疫苗(IPV)(第2剂)',
+                        ],
+                    ],
+                    'right' => [
+                        [
+                            'url' => 1,
+                            'name' => '五联疫苗',
+                        ],
+                    ],
+
+                ]
+            ],
+            [
+                'name' => '四月龄',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=8',
+                            'name' => '无细胞百白破疫苗	',
+                        ],
+                        [
+                            'url' => '/pages/tool/vview/index?id=7',
+                            'name' => '脊灰疫苗(OPV)(第3剂)',
+                        ],
+                    ],
+                    'right' => [
+                        [
+                            'url' => 1,
+                            'name' => '五联疫苗',
+                        ],
+                    ],
+
+                ]
+            ],
+            [
+                'name' => '五月龄',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=9',
+                            'name' => '无细胞百白破疫苗(第3剂)	',
+                        ],
+                    ],
+                    'right' => [],
+
+                ]
+            ],
+            [
+                'name' => '六月龄',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=11',
+                            'name' => 'A群流脑疫苗',
+                        ],
+                        [
+                            'url' => '/pages/tool/vview/index?id=10',
+                            'name' => '乙肝疫苗(第3剂)',
+                        ],
+                    ],
+                    'right' => [],
+
+                ]
+            ],
+            [
+                'name' => '八月龄',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=16',
+                            'name' => '麻腮风疫苗(第1剂)',
+                        ],
+                    ],
+                    'right' => [],
+
+                ]
+            ],
+            [
+                'name' => '九月龄',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=14',
+                            'name' => 'A群流脑疫苗(第2剂)',
+                        ],
+                    ],
+                    'right' => [],
+                ]
+            ],
+            [
+                'name' => '一岁',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=13',
+                            'name' => '乙脑减毒疫苗(第1剂)	',
+                        ],
+                    ],
+                    'right' => [],
+
+                ]
+            ],
+            [
+                'name' => '一岁半',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=21',
+                            'name' => '麻腮风疫苗(第1剂)',
+                        ],
+                        [
+                            'url' => '/pages/tool/vview/index?id=15',
+                            'name' => '无细胞百白破疫苗',
+                        ],
+                        [
+                            'url' => '/pages/tool/vview/index?id=17',
+                            'name' => '甲肝疫苗(第1剂)',
+                        ],
+                    ],
+                    'right' => [],
+
+                ]
+            ],
+            [
+                'name' => '二岁',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=18',
+                            'name' => '乙脑减毒疫苗	',
+                        ],
+                        [
+                            'url' => '/pages/tool/vview/index?id=23',
+                            'name' => '甲肝疫苗(第2剂)',
+                        ],
+                    ],
+                    'right' => [],
+
+                ]
+            ],
+            [
+                'name' => '三岁',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=19',
+                            'name' => 'A+C群流脑疫苗(第1剂)',
+                        ],
+                    ],
+                    'right' => [],
+
+                ]
+            ],
+            [
+                'name' => '四岁',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=20',
+                            'name' => '脊灰疫苗(OPV)(第4剂)',
+                        ],
+
+                    ],
+                    'right' => [],
+
+                ]
+            ],
+            [
+                'name' => '六岁',
+                'vaccines' => [
+                    'left' => [
+                        [
+                            'url' => '/pages/tool/vview/index?id=42',
+                            'name' => 'A+C群流脑疫苗',
+                        ],
+                        [
+                            'url' => '/pages/tool/vview/index?id=22',
+                            'name' => '白破疫苗(DT)',
+                        ],
+                    ],
+                    'right' => [],
+
+                ]
+            ],
+
+        ];
+        //$list = Vaccine::find()->where(['>', 'source', 0])->orderBy('source asc')->all();
+        return array_reverse($ages);
+    }
+}
