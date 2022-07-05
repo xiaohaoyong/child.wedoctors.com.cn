70186076a9191d321259fefa10e6b02194fb281b
wangzhen <xiaohaoyong@126.com>
2022-07-05 16:34:59 +0800

    324

--- console/controllers/FamilyDoctorController.php
+++ console/controllers/FamilyDoctorController.php
@@ -182,8 +182,8 @@ class FamilyDoctorController extends Controller
                         $idcard = $v->field27 ? $v->field27 : $v->idcard;
                         $idcard=str_replace('*','',$idcard);
 
-                        if(!$idcard && $userParent->mother_id){
-                            $idcard=$userParent->mother_id.'(母亲)';
+                        if(!$idcard){
+                            $idcard='';
                         }
                         echo $v->name;
                         echo $idcard;
