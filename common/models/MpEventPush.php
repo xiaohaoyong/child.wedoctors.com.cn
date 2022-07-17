85e4ae9e5d1d80a341a4c7e1473f67f71d217ce7
xiaohaoyong@126.com <xiaohaoyong@126.com>
2022-07-17 12:09:13 +0800

    123

---
+++ common/models/MpEventPush.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "mp_event_push".
+ *
+ * @property int $id
+ * @property string $openid openid
+ * @property int $createtime 创建时间
+ * @property string $event 事件类型
+ * @property int $is_push 是否推送
+ */
+class MpEventPush extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'mp_event_push';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['createtime', 'is_push'], 'required'],
+            [['createtime', 'is_push'], 'integer'],
+            [['openid'], 'string', 'max' => 100],
+            [['event'], 'string', 'max' => 30],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'openid' => 'Openid',
+            'createtime' => 'Createtime',
+            'event' => 'Event',
+            'is_push' => 'Is Push',
+        ];
+    }
+}
