2644007f048f00c937fe47511749797901b02db4
xiaohaoyong@126.com <xiaohaoyong@126.com>
2022-07-22 12:25:29 +0800

    123

--- hospital/views/appoint-calling/text.php
+++ hospital/views/appoint-calling/text.php
@@ -1,15 +1,13 @@
 <br>
 <br><br><br><br><br><br>
-<button onclick="actionList('请张三到第三诊室接种')">点击</button>
+<common-audio ref="audioTip"></common-audio>
+<button onclick="inputForm()">点击</button>
 <script>
-    function actionList(text){
-        alert(text);
-        let msg = new SpeechSynthesisUtterance(text);
-        console.log(msg)
-        //msg.rate = 4 播放语速
-        //msg.pitch = 10 音调高低
-        //msg.text = "播放文本"
-        //msg.volume = 0.5 播放音量
-        speechSynthesis.speak(msg);
+    const synth = window.speechSynthesis;
+
+
+    function inputForm (event) {
+        const utterThis = new SpeechSynthesisUtterance('张三您好');
+        synth.speak(utterThis);
     }
 </script>
