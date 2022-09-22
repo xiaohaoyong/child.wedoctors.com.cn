46dbca75233c14b70ddbe563e4db12aa30d3bf61
xiaohaoyong@126.com <xiaohaoyong@126.com>
2022-09-22 17:19:26 +0800

    123

--- databackend/views/site/index.php
+++ databackend/views/site/index.php
@@ -216,7 +216,7 @@ databackend\assets\IndexAsset::register($this);
                                 <td><?= $v->doctor[0]->name ?></td>
                                 <td><?= $v->parent->father . "/" . $v->parent->mother ?></td>
                                 <td><?= date('Y-m-d', \common\models\DoctorParent::findOne(['parentid' => $v->userid])->createtime) ?></td>
-                                <td><?= $v->parent->mother_phone ? $v->parent->mother_phone : $v->parent->father_phone ? $v->parent->father_phone : $v->parent->field12 ?></td>
+                                <td><?php //$v->parent->mother_phone ? $v->parent->mother_phone : $v->parent->father_phone ? $v->parent->father_phone : $v->parent->field12 ?></td>
                             </tr>
                         <?php } ?>
                     </table>
@@ -227,113 +227,6 @@ databackend\assets\IndexAsset::register($this);
         </div>
 
     </div>
-    <?php if (\Yii::$app->user->identity->type == 1) { ?>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="box">
-                    <div class="box-header">
-                        <h3 class="box-title">社区服务中心签约管理数据</h3>
-                        <div class="box-tools">
-                            <a href="<?= \yii\helpers\Url::to(['user-doctor/index']) ?>">查看更多</a>
-                            <a href="<?= \yii\helpers\Url::to(['site/data']) ?>">下载当日数据</a>
-
-                        </div>
-                    </div>
-                    <!-- /.box-header -->
-                    <div class="box-body no-padding">
-                        <table id="example2" class="table table-bordered table-hover" style="font-size: 12px;">
-                            <thead>
-                            <tr>
-                                <th>社区卫生服务中心</th>
-                                <th>辖区内管理儿童数</th>
-                                <th>今日签约</th>
-                                <th>签约总数</th>
-                                <th>今日宣教</th>
-                                <th>已宣教数</th>
-                                <th>管理服务率</th>
-                                <th>数据</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                            foreach ($doctor as $k => $v) {
-                                ?>
-                                <tr>
-                                    <td><?= $v->name ?></td>
-                                    <td><?= $total = \common\models\ChildInfo::find()->andFilterWhere(['source' => $v->hospitalid])->andFilterWhere(['admin' => $v->hospitalid])->andFilterWhere(['>', 'birthday', strtotime('-3 year')])->count() ?></td>
-                                    <td><?php
-                                        $today = strtotime(date('Y-m-d 00:00:00'));
-                                        //今日已签约
-                                        echo \common\models\ChildInfo::find()
-                                            ->leftJoin('doctor_parent', '`doctor_parent`.`parentid` = `child_info`.`userid`')
-                                            ->andFilterWhere(['`doctor_parent`.`level`' => 1])
-                                            ->andFilterWhere(['`doctor_parent`.`doctorid`' => $doctorid])
-                                            ->andFilterWhere(['child_info.admin'=>$v->hospitalid])
-                                            ->andFilterWhere([">",'`doctor_parent`.createtime',$today])
-                                            ->andFilterWhere(['>','child_info.birthday',strtotime('-3 year')])
-                                            ->count()
-                                        ?>
-                                    </td>
-                                    <td><?php
-                                        //已签约总数
-                                        echo $q = \common\models\ChildInfo::find()
-                                            ->leftJoin('doctor_parent', '`doctor_parent`.`parentid` = `child_info`.`userid`')
-                                            ->andFilterWhere(['`doctor_parent`.doctorid' => $v->userid])
-                                            ->andFilterWhere(['>', 'child_info.birthday', strtotime('-3 year')])
-                                            ->andFilterWhere(['child_info.admin' => $v->hospitalid])
-                                            ->andFilterWhere(['`doctor_parent`.level' => 1])->count();
-                                        ?>
-                                    </td>
-                                    <td><?php
-                                        $today = strtotime(date('Y-m-d 00:00:00'));
-                                        //今日宣教
-                                        echo \common\models\ArticleUser::find()
-                                            ->andFilterWhere(['userid' => $v->userid])
-                                            ->andFilterWhere([">", 'createtime', $today])->count();
-                                        ?></td>
-                                    <td><?php
-                                        //已宣教
-                                        echo \common\models\ArticleUser::find()
-                                            ->andFilterWhere(['userid' => $v->userid])->count();
-                                        ?></td>
-                                    <td>
-                                        <?php
-                                        if ($total) {
-                                            $baifen = round(($q / $total) * 100, 1);
-                                        } else {
-                                            $baifen = 0;
-                                        }
-
-                                        if ($baifen > 44) {
-
-                                            $color = 'bg-green';
-                                            $color1 = 'progress-bar-success';
-                                        } elseif ($baifen > 34) {
-                                            $color = 'bg-yellow';
-                                            $color1 = 'progress-bar-yellow';
-                                        } else {
-                                            $color = 'bg-red';
-                                            $color1 = 'progress-bar-danger';
-                                        }
-
-                                        ?>
-                                        <div class="progress progress-xs">
-                                            <div class="progress-bar <?= $color1 ?>"
-                                                 style="width: <?= $baifen ?>%"></div>
-                                        </div>
-                                    </td>
-                                    <td><span class="badge <?= $color ?>"><?= $baifen ?>%</span></td>
-                                </tr>
-                            <?php } ?>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
-            </div>
-        </div>
-    <?php } ?>
 
     <?php if (\Yii::$app->user->identity->type == 1) { ?>
 
