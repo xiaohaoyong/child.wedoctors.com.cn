278695b739ebb11d249b82ba14177ca62ae41af5
xiaohaoyong@126.com <xiaohaoyong@126.com>
2022-09-22 15:08:30 +0800

    123

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,9 @@
 <?php
 $config= [
-//    'aliases' => [
-//        '@bower' => '@vendor/bower-asset',
-//        '@npm'   => '@vendor/npm-asset',
-//    ],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'language' => 'zh-CN',
     'timeZone' => 'Asia/Shanghai',
