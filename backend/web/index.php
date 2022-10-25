03b5f565efb4bf272f10716f4f2497b1d2d9d493
liruishu <liruishu@xywy.com>
2022-10-25 14:55:24 +0800

    c

--- backend/web/index.php
+++ backend/web/index.php
@@ -2,7 +2,7 @@
 //phpinfo();
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
+define('YII_DEBUG', true);
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
