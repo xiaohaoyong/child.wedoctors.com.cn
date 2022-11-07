840ed809708250d5508bd929300cff55d851236f
root <root@xywy.com>
2022-11-07 15:54:41 +0800

    m

---
+++ backend/controllers/SigningRecordController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use common\models\SigningRecord;
+    use app\models\SigningRecordSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+* SigningRecordController implements the CRUD actions for SigningRecord model.
+*/
+class SigningRecordController extends Controller
+{
+/**
+* @inheritdoc
+*/
+public function behaviors()
+{
+return [
+'verbs' => [
+'class' => VerbFilter::className(),
+'actions' => [
+'delete' => ['POST'],
+],
+],
+];
+}
+
+/**
+* Lists all SigningRecord models.
+* @return mixed
+*/
+public function actionIndex()
+{
+    $searchModel = new SigningRecordSearch();
+    $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+    return $this->render('index', [
+    'searchModel' => $searchModel,
+    'dataProvider' => $dataProvider,
+    ]);
+}
+
+/**
+* Displays a single SigningRecord model.
+* @param integer $id
+* @return mixed
+*/
+public function actionView($id)
+{
+return $this->render('view', [
+'model' => $this->findModel($id),
+]);
+}
+
+/**
+* Creates a new SigningRecord model.
+* If creation is successful, the browser will be redirected to the 'view' page.
+* @return mixed
+*/
+public function actionCreate()
+{
+$model = new SigningRecord();
+
+if ($model->load(Yii::$app->request->post()) && $model->save()) {
+return $this->redirect(['view', 'id' => $model->id]);
+} else {
+return $this->render('create', [
+'model' => $model,
+]);
+}
+}
+
+/**
+* Updates an existing SigningRecord model.
+* If update is successful, the browser will be redirected to the 'view' page.
+* @param integer $id
+* @return mixed
+*/
+public function actionUpdate($id)
+{
+$model = $this->findModel($id);
+
+if ($model->load(Yii::$app->request->post()) && $model->save()) {
+return $this->redirect(['view', 'id' => $model->id]);
+} else {
+return $this->render('update', [
+'model' => $model,
+]);
+}
+}
+
+/**
+* Deletes an existing SigningRecord model.
+* If deletion is successful, the browser will be redirected to the 'index' page.
+* @param integer $id
+* @return mixed
+*/
+public function actionDelete($id)
+{
+$this->findModel($id)->delete();
+
+return $this->redirect(['index']);
+}
+
+/**
+* Finds the SigningRecord model based on its primary key value.
+* If the model is not found, a 404 HTTP exception will be thrown.
+* @param integer $id
+* @return SigningRecord the loaded model
+* @throws NotFoundHttpException if the model cannot be found
+*/
+protected function findModel($id)
+{
+if (($model = SigningRecord::findOne($id)) !== null) {
+return $model;
+} else {
+throw new NotFoundHttpException('The requested page does not exist.');
+}
+}
+}
