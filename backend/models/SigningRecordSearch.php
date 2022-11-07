21e8b8c0a44c1783684f52e9b3a1ad9d675a3b63
root <root@xywy.com>
2022-11-07 15:56:09 +0800

    m

---
+++ backend/models/SigningRecordSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\SigningRecord;
+
+/**
+ * SigningRecordSearch represents the model behind the search form about `common\models\SigningRecord`.
+ */
+class SigningRecordSearch extends SigningRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'userid', 'type', 'sign_item_id_from', 'sign_item_id_to', 'status', 'createtime'], 'integer'],
+            [['info_pics', 'remark'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = SigningRecord::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'userid' => $this->userid,
+            'type' => $this->type,
+            'sign_item_id_from' => $this->sign_item_id_from,
+            'sign_item_id_to' => $this->sign_item_id_to,
+            'status' => $this->status,
+            'createtime' => $this->createtime,
+        ]);
+
+        $query->andFilterWhere(['like', 'info_pics', $this->info_pics])
+            ->andFilterWhere(['like', 'remark', $this->remark]);
+        $query->orderBy([self::primaryKey()[0]=>SORT_DESC]);
+
+        return $dataProvider;
+    }
+}
---
+++ common/models/SigningRecord.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "signing_record".
+ *
+ * @property int $id
+ * @property int $userid 家庭ID
+ * @property int $type 成员类型，1-孕妇  2-孩子
+ * @property int $sign_item_id_from 原签约团队id
+ * @property int $sign_item_id_to 变更后的签约团队id
+ * @property int $status 状态 0-未审核 1-审核通过 2-审核不通过
+ * @property string $info_pics 资料图片
+ * @property string $remark 备注
+ * @property int $createtime 创建时间
+ */
+class SigningRecord extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'signing_record';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['userid', 'type', 'sign_item_id_from', 'sign_item_id_to', 'status', 'createtime'], 'integer'],
+            [['info_pics'], 'string', 'max' => 500],
+            [['remark'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'userid' => '家庭ID',
+            'type' => '成员类型，1-孕妇  2-孩子',
+            'sign_item_id_from' => '原签约团队id',
+            'sign_item_id_to' => '变更后的签约团队id',
+            'status' => '状态 0-未审核 1-审核通过 2-审核不通过',
+            'info_pics' => '资料图片',
+            'remark' => '备注',
+            'createtime' => '创建时间',
+        ];
+    }
+}
