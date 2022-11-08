32fbea89adc90b57ae17e4dda1425c57c6736087
unknown <zhangzhenyu@xywy.com>
2022-11-08 08:01:53 +0800

    m

--- common/models/SigningRecord.php
+++ common/models/SigningRecord.php
@@ -73,7 +73,7 @@ class SigningRecord extends \yii\db\ActiveRecord
 
     public function get_child_info($cid)
     {
-        return ChildInfo::findOne($cid);
+        return ChildInfo::findOne($cid)->asArray();
     }
 
     public function get_mom_info($userid)
