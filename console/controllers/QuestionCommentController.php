6ae389f487c0aad65587e37173b47cfe17b5d6ef
liruishu <liruishu@xywy.com>
2022-11-08 14:14:58 +0800

    c

--- console/controllers/QuestionCommentController.php
+++ console/controllers/QuestionCommentController.php
@@ -39,14 +39,14 @@ class QuestionCommentController extends \yii\console\Controller
                                 Question::updateAll(['state'=>2],['id'=>$val['id']]);
 
                                 $userDoctor = UserDoctor::find()->where(['userid'=>$val['doctorid']])->one();
-                                $thing2 = '感谢你的配合';
-                                $thing1 = $userDoctor->name;
+                                $thing1 = '在线咨询';
+                               // $thing2 = '您向'.$userDoctor->name.'的在线咨询已结束，邀请您对医生的回复进行评价';
+                                $thing2 = '邀请您对本次咨询进行评价';
                                 $data = [
                                     'thing1' => ARRAY('value' => $thing1),
                                     'thing2' => ARRAY('value' => $thing2),
                                     'time3' => ARRAY('value' => date('Y年m月d日 H:i',time())),
                                 ];
-                                var_dump($data);
                                 $userLogin = UserLogin::find()->where(['userid'=>$val['userid']])->one();
                                 $rs=WechatSendTmp::sendSubscribe($data,$userLogin->xopenid,'cJqc11RdX95akxICJmQo3nP-0yo6VA4eHAeZHjEViHo','/pages/question/view?id='.$val['id']);
 
