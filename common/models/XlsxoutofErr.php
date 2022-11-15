6db62e4f3d5c3bb69c035288205d7bdd31ea737c
liuzeng <liuzeng@xywy.com>
2022-11-15 13:40:47 +0800

    add xlsx

---
+++ common/models/XlsxoutofErr.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+
+class XlsxoutofErr extends \yii\db\ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'xlsxoutof_err';
+    }
+}
