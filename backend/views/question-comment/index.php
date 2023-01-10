c7ead9e02b2bec117c3670f73c6ed55afb48ac3e
liruishu <liruishu@xywy.com>
2023-01-10 11:50:18 +0800

    c

--- backend/views/question-comment/index.php
+++ backend/views/question-comment/index.php
@@ -48,8 +48,9 @@ $this->params['breadcrumbs'][] = $this->title;
                                 [
                                     'attribute' => '指定社区',
                                     'value' => function ($e) {
-                                        $doctor=\common\models\UserDoctor::find()->where(['userid'=>$e->userid])->one();
+                                        $doctor=\common\models\UserDoctor::find()->where(['userid'=>$e->doctorid])->one();
                                         return $doctor->name;
+
                                     }
                                 ],
 
