d084a219dec96807b6758fb633aa26683348411f
liruishu <liruishu@xywy.com>
2023-01-10 15:18:54 +0800

    问题列表

--- backend/models/QuestionSearch.php
+++ backend/models/QuestionSearch.php
@@ -12,14 +12,21 @@ use common\models\Question;
  */
 class QuestionSearch extends Question
 {
+    public $startDate;
+    public $endDate;
     /**
      * @inheritdoc
      */
     public function rules()
     {
+
         return [
-            [['id', 'userid', 'createtime', 'childid', 'doctorid', 'orderid', 'level', 'state'], 'integer'],
+            [['id', 'userid', 'createtime', 'childid', 'doctorid', 'orderid', 'level', 'state','is_comment'], 'integer'],
+            [['startDate','endDate'], 'date', 'format' => 'php:Y-m-d', 'message'=>'日期格式不对'],
+            // ['state','default','value'=>2]
+
         ];
+
     }
 
     /**
@@ -56,6 +63,24 @@ class QuestionSearch extends Question
             return $dataProvider;
         }
 
+        if($this->state == ''){
+            $this->state = -1;
+        }
+        if($this->is_comment == ''){
+            $this->is_comment = -1;
+        }
+
+        if($this->state == -1){
+            $query->andWhere(['in','state',[0,1,2]]);
+        }else{
+            $query->andWhere(['state'=>$this->state]);
+        }
+        if($this->is_comment == -1){
+            $query->andWhere(['in','is_comment',[0,1]]);
+        }else{
+            $query->andWhere(['is_comment'=>$this->is_comment]);
+        }
+
         // grid filtering conditions
         $query->andFilterWhere([
             'id' => $this->id,
@@ -65,10 +90,29 @@ class QuestionSearch extends Question
             'doctorid' => $this->doctorid,
             'orderid' => $this->orderid,
             'level' => $this->level,
-            'state' => $this->state,
+            //'state' => $this->state,
         ]);
+        if ($this->startDate !== '' and $this->endDate !== null) {
+
+            $state = strtotime($this->startDate . " 00:00:00");
+            $end = strtotime($this->endDate . " 23:59:59");
+            $query->andFilterWhere(['>', '`createtime`', $state]);
+            $query->andFilterWhere(['<', '`createtime`', $end]);
+        }
         $query->orderBy([self::primaryKey()[0]=>SORT_DESC]);
+       // echo $query->createCommand()->getRawSql();
 
         return $dataProvider;
+    }
+    public function attributeLabels()
+    {
+        $attr = parent::attributeLabels();
+
+
+        $attr['startDate'] = '创建时间';
+        $attr['endDate'] = '~';
+        // $attr['county'] = '区/县';
+
+        return $attr;
     }
 }
--- backend/views/question/_search.php
+++ backend/views/question/_search.php
@@ -19,8 +19,16 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'id') ?>
 
     <?= $form->field($model, 'userid') ?>
-
-    <?= $form->field($model, 'createtime') ?>
+    <?= $form->field($model, 'startDate')->widget(\kartik\date\DatePicker::className(), ['pluginOptions' => [
+        'format' => 'yyyy-mm-dd',
+        'autocomplete'=>'off',
+        'todayHighlight' => true
+    ]]) ?>
+    <?= $form->field($model, 'endDate')->widget(\kartik\date\DatePicker::className(), ['pluginOptions' => [
+        'format' => 'yyyy-mm-dd',
+        'autocomplete'=>'off',
+        'todayHighlight' => true
+    ]]) ?>
 
     <?= $form->field($model, 'childid') ?>
 
@@ -31,6 +39,8 @@ use yii\widgets\ActiveForm;
     <?php // echo $form->field($model, 'level') ?>
 
     <?php // echo $form->field($model, 'state') ?>
+    <?=$form->field($model,'state')->dropDownList(\common\models\Question::$stateText,['prompt'=>'全部'])->label('问题状态');?>
+    <?=$form->field($model,'is_comment')->dropDownList(['1'=>'是','0'=>'否'],['prompt'=>'全部'])->label('是否评价');?>
 
     <div class="form-group">
         <?= Html::submitButton('搜索', ['class' => 'btn btn-primary']) ?>
