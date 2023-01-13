a07beee3cedcf36e41a47b658c632601034d5491
liruishu <liruishu@xywy.com>
2023-01-13 12:02:02 +0800

    c

--- backend/views/appoint-comment/view.php
+++ backend/views/appoint-comment/view.php
@@ -46,7 +46,7 @@ $this->params['breadcrumbs'][] = $this->title;
                             'attribute' => '预约时间',
                             'value' => function ($e) {
                                 $info = \common\models\Appoint::findOne(['id' => $e->aid]);
-                                return date("Y-m-d H:i:s",$info->appoint_time);
+                                return date("Y-m-d H:i:s",$info->appoint_date);
                             }
                         ],
                         [
