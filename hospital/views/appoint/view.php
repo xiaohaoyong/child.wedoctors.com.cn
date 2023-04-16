57ee8344c39596d02419e8af23bab6c9dc076e4a
wangzhen <xiaohaoyong@126.com>
2023-04-17 01:03:10 +0800

    23

--- hospital/views/appoint/view.php
+++ hospital/views/appoint/view.php
@@ -33,6 +33,8 @@ $this->params['breadcrumbs'][] = $this->title;
                     ],
                 ]) ?>
                 <div class="form-group" style="padding-top: 20px;">
+                    <?= Html::a('通过', ['done?state=1&id='.$model->id],
+                        ['class' => 'btn btn-primary']) ?>
                     <?= Html::a('不通过', ['done?state=3&id='.$model->id],
                         ['class' => 'btn btn-danger']) ?>
                 </div>
