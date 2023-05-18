39ea32226ff133086515ca89fa9dacace019d1ce
xiaohaoyong@126.com <xiaohaoyong@126.com>
2023-05-18 17:29:14 +0800

    123

--- console/controllers/FiveController.php
+++ console/controllers/FiveController.php
@@ -67,8 +67,8 @@ class FiveController extends Controller
 
     public function actionExcel()
     {
-        $stime=strtotime('2023-02-01');
-        $etime=strtotime('2023-03-01');
+        $stime=strtotime('2023-03-01');
+        $etime=strtotime('2023-04-01');
         $userDoctor = UserDoctor::find()->all();
         foreach ($userDoctor as $k=>$v){
             $rs=[];
