923e761fe064877d3f5509c99efcd4a4b8024ae5
xiaohaoyong@126.com <xiaohaoyong@126.com>
2023-05-26 13:01:39 +0800

    123

--- hospital/controllers/DoctorTeamController.php
+++ hospital/controllers/DoctorTeamController.php
@@ -185,7 +185,7 @@ class DoctorTeamController extends Controller
 
                 }
                 if($i<1){
-                    \Yii::$app->getSession()->setFlash('success', '未查询到儿童请检查上传文件是否正确，需要字段有："儿童姓名，出生日期，母亲姓名"');
+                    \Yii::$app->getSession()->setFlash('error', '未查询到儿童请检查上传文件是否正确，需要字段有："儿童姓名，出生日期，母亲姓名"');
                 }else {
                     \Yii::$app->getSession()->setFlash('success', '处理完成共匹配成功"' . $i . '"个儿童');
                 }
