96046356efb705a3f9a703aef1b1dc4543627286
xiaohaoyong@126.com <xiaohaoyong@126.com>
2023-05-26 18:57:53 +0800

    123

--- frontend/views/wappoint/list.php
+++ frontend/views/wappoint/list.php
@@ -3,10 +3,8 @@ $app = \EasyWeChat\Factory::officialAccount(\Yii::$app->params['easywechat']);
 $cache = new \common\helpers\EasyRedisCache();
 
 
-$app->rebind('cache', $cache);
 ?>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.6.0.js"></script>
-    wx.config(<?=$app->jssdk->buildConfig(['wx-open-launch-weapp', true]);?>);
 
 <script>
 
