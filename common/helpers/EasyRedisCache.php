7a107e5a0d19fecb32c462529060066aa71fc0f2
xiaohaoyong@126.com <xiaohaoyong@126.com>
2023-05-26 20:01:43 +0800

    123

--- common/helpers/EasyRedisCache.php
+++ common/helpers/EasyRedisCache.php
@@ -18,54 +18,150 @@ class EasyRedisCache implements \Psr\SimpleCache\CacheInterface
         $this->redis=\Yii::$app->rdmp;
     }
 
-    public function get($key, $default = null)
-    {
-        $value = $this->redis->get($key);
-        return explode(',',$value);
-        // your code
+    /**
+     * @param string $key
+     * @param null $default
+     * @return InvalidArgumentException|bool|null|string
+     */
+    public function get($key, $default = null) {
+        if (!is_string($key)) {
+            return new InvalidArgumentException("In get key must be string");
+        }
+
+        if ($this->redis->exists($key)) {
+            return $this->redis->get($key);
+        }
+
+        return $default;
     }
 
-    public function set($key, $value, $ttl = null)
-    {
-        $value=implode(',',$value);
-        $this->redis->set($key,$value);
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param null|int $ttl
+     * @return InvalidArgumentException|bool
+     */
+    public function set($key, $value, $ttl = null) {
+        if (!is_string($key)) {
+            return new InvalidArgumentException("In get key must be string");
+        }
 
-        // your code
+        if (empty($ttl)) {
+            return $this->redis->set($key, $value);
+        }
+
+        return $this->redis->psetex($key, $ttl, $value);
     }
 
-    public function delete($key)
-    {
-        $this->redis->delete($key);
+    /**
+     * @param string $key
+     * @return InvalidArgumentException|bool
+     */
+    public function delete($key) {
+        if (!is_string($key)) {
+            return new InvalidArgumentException("In get key must be string");
+        }
 
-        // your code
+        $numberDeleted = $this->redis->del($key);
+
+        if ($numberDeleted < 1) {
+            return false;
+        }
+
+        return true;
     }
 
-    public function clear()
-    {
-        // your code
+    /**
+     * @return bool
+     */
+    public function clear() {
+        return $this->redis->flushAll();
     }
 
-    public function getMultiple($keys, $default = null)
-    {
-        var_dump('getMultiple');exit;
+    /**
+     * @param iterable $keys
+     * @param null $default
+     * @return InvalidArgumentException|array
+     */
+    public function getMultiple($keys, $default = null) {
+        if (!is_array($keys)) {
+            return new InvalidArgumentException("Keys mast be array");
+        }
 
-        // your code
+        foreach ($keys as $valueKey) {
+            if (!is_string($valueKey)) {
+                return new InvalidArgumentException("All keys in keyArray must be string");
+            }
+        }
+
+        $values = $this->redis->mget($keys);
+        foreach ($values as $key => $value) {
+            if ($value === false) {
+                $values[$key] = $default;
+            }
+        }
+
+        return array_combine($keys, $values);
     }
 
-    public function setMultiple($values, $ttl = null)
-    {
-        // your code
+    /**
+     * @param iterable $values
+     * @param null $ttl
+     * @return InvalidArgumentException|bool
+     */
+    public function  setMultiple($values, $ttl = null) {
+        if (!is_array($values)) {
+            return new InvalidArgumentException('Values must be array');
+        }
+
+        $result = true;
+
+        if (isset($ttl)) {
+            foreach ($values as $key => $value) {
+                $result = $result && ($this->redis->psetex($key, $ttl, $value));
+            }
+
+            return $result;
+        }
+
+        return $this->redis->mset($values);
     }
 
-    public function deleteMultiple($keys)
-    {
-        // your code
+    /**
+     * @param iterable $keys
+     * @return InvalidArgumentException|bool
+     */
+    public function deleteMultiple($keys) {
+        if (!is_array($keys)) {
+            return new InvalidArgumentException('Keys must be array');
+        }
+
+        $countDeletedValues = 0;
+
+        foreach ($keys as $value) {
+            if (!is_string($value)) {
+                return new InvalidArgumentException('All value in keys must be string');
+            }
+
+            $countDeletedValues += $this->redis->del($value);
+        }
+
+        if ($countDeletedValues === count($keys)) {
+            return true;
+        }
+
+        return false;
     }
 
-    public function has($key)
-    {
-        $value = $this->redis->get($key);
-        return explode(',',$value);
-        // your code
+    /**
+     * @param string $key
+     * @return InvalidArgumentException|bool
+     */
+    public function has($key) {
+        if (!is_string($key)) {
+            return new InvalidArgumentException('Key must be array');
+        }
+
+        return $this->redis->exists($key);
     }
 }
--- frontend/views/wappoint/test1.php
+++ frontend/views/wappoint/test1.php
@@ -1,9 +1,5 @@
 <?php
 $app = \EasyWeChat\Factory::officialAccount(\Yii::$app->params['easywechat']);
-$cache = new \common\helpers\EasyRedisCache();
-
-
-$app->rebind('cache', $cache);
 ?>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.6.0.js"></script>
 <script>
