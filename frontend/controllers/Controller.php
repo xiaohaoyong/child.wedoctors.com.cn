bc78b12b62ea3f3e3ebf947bbe17eea6f46359be
xiaohaoyong@126.com <xiaohaoyong@126.com>
2023-05-29 14:47:57 +0800

    123

--- frontend/controllers/Controller.php
+++ frontend/controllers/Controller.php
@@ -36,53 +36,54 @@ class Controller extends \yii\web\Controller
         {
             return true;
         }
-        $config=\Yii::$app->params['wechat'];
-        $config['oauth']['callback']=\Yii::$app->request->url;
-        $app = Factory::officialAccount($config);
-        $oauth = $app->oauth;
-        if(\Yii::$app->request->get('code') && !$_SESSION['wechat_user']) {
-            $wechat_user=$oauth->user();
-            if($wechat_user){
-                $_SESSION['wechat_user']=$wechat_user;
-            }
-        }
-// 未登录
-        if (!$_SESSION['wechat_user'] && !\Yii::$app->request->get('code')) {
-
-            $oauth->redirect()->send();
-            // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
-            // $oauth->redirect()->send();
-        }
-        $wechat_user=$_SESSION['wechat_user'];
-        $openid=$wechat_user['original']['openid'];
-        $unionid=$wechat_user['original']['unionid'];
-        $query=UserLogin::find()->where(['openid' => $openid]);
-//        if($unionid){
-//            $query->orWhere(['and', ['unionid' => $unionid]]);
+        $this->login=UserLogin::find()->where(['userid'=>1292985])->one();
+//        $config=\Yii::$app->params['wechat'];
+//        $config['oauth']['callback']=\Yii::$app->request->url;
+//        $app = Factory::officialAccount($config);
+//        $oauth = $app->oauth;
+//        if(\Yii::$app->request->get('code') && !$_SESSION['wechat_user']) {
+//            $wechat_user=$oauth->user();
+//            if($wechat_user){
+//                $_SESSION['wechat_user']=$wechat_user;
+//            }
 //        }
-        $query->andWhere(['type'=>0]);
-        $login=$query->one();
-        if($login){
-            $this->login=$login;
-        }elseif($openid){
-            $user=new User();
-            $user->type=0;
-            $user->level=1;
-            $user->phone=0;
-            $user->source=3;
-            if($user->save()){
-                $login=new UserLogin();
-                $login->userid=$user->id;
-                $login->phone=0;
-                $login->openid=$openid;
-                $login->unionid=$unionid;
-                $login->type=0;
-                $login->save();
-                $this->login=$login;
-            }else{
-                return false;
-            }
-        }
+//// 未登录
+//        if (!$_SESSION['wechat_user'] && !\Yii::$app->request->get('code')) {
+//
+//            $oauth->redirect()->send();
+//            // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
+//            // $oauth->redirect()->send();
+//        }
+//        $wechat_user=$_SESSION['wechat_user'];
+//        $openid=$wechat_user['original']['openid'];
+//        $unionid=$wechat_user['original']['unionid'];
+//        $query=UserLogin::find()->where(['openid' => $openid]);
+////        if($unionid){
+////            $query->orWhere(['and', ['unionid' => $unionid]]);
+////        }
+//        $query->andWhere(['type'=>0]);
+//        $login=$query->one();
+//        if($login){
+//            $this->login=$login;
+//        }elseif($openid){
+//            $user=new User();
+//            $user->type=0;
+//            $user->level=1;
+//            $user->phone=0;
+//            $user->source=3;
+//            if($user->save()){
+//                $login=new UserLogin();
+//                $login->userid=$user->id;
+//                $login->phone=0;
+//                $login->openid=$openid;
+//                $login->unionid=$unionid;
+//                $login->type=0;
+//                $login->save();
+//                $this->login=$login;
+//            }else{
+//                return false;
+//            }
+//        }
         return true;
     }
 }
