7fcd29c494b75d5fb8696c4a2760a4fee7bd988b
xiaohaoyong@126.com <xiaohaoyong@126.com>
2023-06-16 10:13:49 +0800

    123

--- console/controllers/ChildInfoController.php
+++ console/controllers/ChildInfoController.php
@@ -89,7 +89,7 @@ class ChildInfoController extends \yii\console\Controller
             ->andFilterWhere(['`child_info`.`admin`' =>$userDoctor->hospitalid])
             ->andFilterWhere(['>', '`child_info`.birthday', strtotime('-3 year')])
             ->orderBy("`doctor_parent`.`createtime` desc")
-            ->asArray()->all();
+            ->all();
         var_dump($data);
 //写入内容
 
