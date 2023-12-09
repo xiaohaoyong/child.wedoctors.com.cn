0af52b0690deda6fe6d2cb6de8b14b654f138ff9
wangzhen <xiaohaoyong@126.com>
2023-12-09 17:39:55 +0800

    123

--- frontend/controllers/WappointController.php
+++ frontend/controllers/WappointController.php
@@ -70,7 +70,7 @@ class WappointController extends Controller
             $query->andWhere(['in','userid',$doctorids]);
         }
         if($vaccine == 3){
-            $doctors = $query->andWhere(['!=','userid',47156])->orderBy(["FIELD(userid,1301729,807791,".implode(',',$doctorids).")"=>true])->all();
+            $doctors = $query->andWhere(['!=','userid',47156])->orderBy(["FIELD(userid,807791,1301729,".implode(',',$doctorids).")"=>true])->all();
         }elseif ($search || $county || $type) {
             $doctors = $query->orderBy('appoint desc')->all();
         } else {
