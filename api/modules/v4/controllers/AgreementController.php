fc8da0724b308ec6360b0d608e9ea0b2c5e316b8
wangzhen <xiaohaoyong@126.com>
2024-05-17 16:03:53 +0800

    123

--- api/modules/v4/controllers/AgreementController.php
+++ api/modules/v4/controllers/AgreementController.php
@@ -3,12 +3,22 @@ namespace api\modules\v4\controllers;
 
 use api\controllers\Controller;
 use common\models\Agreement;
+use common\models\DoctorParent;
+use common\models\UserDoctor;
 
 class AgreementController extends Controller
 {
     public function actionContent(){
-        $agreement = Agreement::find()->one();
-        
+
+        $doctorParent = DoctorParent::findOne(['parentid' => $this->userid]);
+        $doctor = UserDoctor::findOne(['userid' => $doctorParent->doctorid]);
+        $agreement = Agreement::find()->where(['doctorid'=>$doctor->userid])->one();
+        if(!$agreement){
+            $agreement = Agreement::find()->where(['county'=>$doctor->county])->one();
+            if(!$agreement){
+                $agreement = Agreement::find()->where(['county'=>0])->andWhere(['doctorid'=>0])->orderBy('id desc')->one();
+            }
+        }
         return ['content'=>$agreement->content];
     }
 
