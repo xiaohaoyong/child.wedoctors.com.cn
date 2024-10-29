508ced5394c9718fd2e987ad0527fac8572ff3ca
xiaohaoyong@126.com <xiaohaoyong@126.com>
2024-10-29 19:42:19 +0800

    BUG

--- weixin/controllers/SuiteController.php
+++ weixin/controllers/SuiteController.php
@@ -47,8 +47,12 @@ class SuiteController extends Controller
                         $scene = str_replace('qrscene_', '', $message['EventKey']);
                         $qrcodeid = Qrcodeid::findOne(['qrcodeid' => $scene]);
                         $doctor_id = $qrcodeid->mappingid;
-                        return $doctor_id;
-                        $weOpenid = WeOpenid::action($message);
+                        try {
+                            $weOpenid = WeOpenid::action($message);
+                        }catch (\Exception $e){
+                            return  $e->getMessage();
+                        }
+                        return $weOpenid;
                         if ($doctor_id) {
                             //扫描社区医院二维码操作
                             $userid = UserLogin::findOne(['openid' => $openid])->userid;
