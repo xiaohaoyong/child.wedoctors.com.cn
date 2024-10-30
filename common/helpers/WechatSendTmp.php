d5641d01e52202e3a15d396b34addd915780f2fc
xiaohaoyong@126.com <xiaohaoyong@126.com>
2024-10-30 11:15:29 +0800

    BUG

--- common/helpers/WechatSendTmp.php
+++ common/helpers/WechatSendTmp.php
@@ -44,21 +44,21 @@ class WechatSendTmp
         return $mpWechat->sendMessage($data);
     }
 
-//    public static function sendX($data,$touser,$tmpid,$url="",$formid){
-////        $push_data['data'] = $data;
-////        $push_data['touser'] = $touser;
-////        $push_data['template_id'] = $tmpid;
-////        $push_data['page'] = $url;
-////        $push_data['form_id']=$formid;
-////
-////        $mpWechat = new \common\vendor\MpWechat([
-////            'token' => \Yii::$app->params['WeToken'],
-////            'appId' => \Yii::$app->params['wxXAppId'],
-////            'appSecret' => \Yii::$app->params['wxXAppSecret'],
-////            'encodingAesKey' => \Yii::$app->params['encodingAesKey'],
-////        ]);
-////        return $mpWechat->sendTemplateMessage($push_data);
-//    }
+    public static function sendX($data,$touser,$tmpid,$url="",$formid){
+//        $push_data['data'] = $data;
+//        $push_data['touser'] = $touser;
+//        $push_data['template_id'] = $tmpid;
+//        $push_data['page'] = $url;
+//        $push_data['form_id']=$formid;
+//
+//        $mpWechat = new \common\vendor\MpWechat([
+//            'token' => \Yii::$app->params['WeToken'],
+//            'appId' => \Yii::$app->params['wxXAppId'],
+//            'appSecret' => \Yii::$app->params['wxXAppSecret'],
+//            'encodingAesKey' => \Yii::$app->params['encodingAesKey'],
+//        ]);
+        return false;
+    }
     public static function sendSubscribe($data,$touser,$tmpid,$url=""){
         $push_data['data'] = $data;
         $push_data['touser'] = $touser;
