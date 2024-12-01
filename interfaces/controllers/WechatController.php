23c918a7de0a590590621b6ff59ba4a487ee7e10
汪振 <9077410+xiaohaoyong@user.noreply.gitee.com>
2024-12-01 12:03:44 +0800

    获取token

--- interfaces/controllers/WechatController.php
+++ interfaces/controllers/WechatController.php
@@ -8,7 +8,7 @@ use yii\web\Response;
 
 class WechatController extends Controller
 {
-    public function getAccessToken()
+    public function actionGetAccessToken()
     {
         \Yii::$app->response->format = Response::FORMAT_JSON;
         $mpWechat = new MpWechat(['token' => \Yii::$app->params['WeToken'], 'appId' => \Yii::$app->params['AppID'], 'appSecret' => \Yii::$app->params['AppSecret'], 'encodingAesKey' => \Yii::$app->params['encodingAesKey']]);
