a0ff0b274949a49691a9d47fd4b012507eb502f9
汪振 <9077410+xiaohaoyong@user.noreply.gitee.com>
2024-12-22 12:54:57 +0800

    获取token

--- frontend/views/wappoint/list.php
+++ frontend/views/wappoint/list.php
@@ -117,3 +117,38 @@ wx.config(<?=json_encode($mpWechat->jsApiConfig(['openTagList'=>['wx-open-launch
 ]);
 \yii\bootstrap\Modal::end();
 ?>
+<!---->
+<!---->
+<!--<div class="wrapper appoint_list">-->
+<!--    <div class="content-wrapper">-->
+<!--        <style>-->
+<!--            .modal_title text {-->
+<!--                font-weight: bold;-->
+<!--                font-size: 16px-->
+<!--            }-->
+<!--            .modal_body{line-height: 24px;}-->
+<!--            .rad{color: rgba(240,85,70,1); font-size: 14px; margin-top: 10px;}-->
+<!--            .button{-->
+<!--                width:179px;-->
+<!--                height:41px;-->
+<!--                background:rgba(240,85,70,1);-->
+<!--                border-radius:20px;-->
+<!--                text-align: center;-->
+<!--                line-height: 41px;-->
+<!--                color: #ffffff;-->
+<!--                display: block;-->
+<!--                margin: 0 auto;-->
+<!--                margin-top: 20px;-->
+<!--            }-->
+<!--        </style>-->
+<!---->
+<!--        <div class="fo" style="margin-top: 100px">-->
+<!--            <a class="button" href="weixin://dl/business/?appid=wx240286cc3d77ba35&path=pages/appoint/list" >-->
+<!--                点击进入小程序预约-->
+<!--            </a>-->
+<!--            <a class="button" href="/wappoint/my" >-->
+<!--                查看历史预约记录-->
+<!--            </a>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
