4f900071fa8fa9231b0769e3a9b9284426017ccf
xiaohaoyong@126.com <xiaohaoyong@126.com>
2025-01-10 17:49:06 +0800

    医生端H5

--- frontend/controllers/TransferController.php
+++ frontend/controllers/TransferController.php
@@ -56,6 +56,7 @@ class TransferController extends Controller
        if($authorization) {
            $curl->setHeader('Authorization', $authorization);
        }
+       echo $path;exit;
        $userJson = $curl->$method();
        return json_decode($userJson,true);
    }
