e7147eed9a3e562c5a5a9a88f63209b355c9cfce
xiaohaoyong@126.com <xiaohaoyong@126.com>
2025-02-06 14:58:07 +0800

    医生端H5

---
+++ api/modules/v4/controllers/SystemController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangzhen
+ * Date: 2021/7/9
+ * Time: 4:23 PM
+ */
+
+namespace backend\modules\v4\controllers;
+
+
+use api\controllers\Controller;
+use common\models\DoctorTeam;
+use common\models\DoctorParent;
+use common\models\TempUserid;
+use common\models\UserDoctor;
+use common\models\UserParent;
+use common\models\ChildInfo;
+
+use common\models\UserLogin;
+
+class SystemController extends Controller
+{
+    public function actionDisable()
+    {
+        $userid = $this->userid;
+        $tempUserid=TempUserid::findOne(['userid'=>$userid]);
+        if($tempUserid){
+            return ['visible'=>1];
+        }else{
+            return ['visible'=>0];
+        }
+    }
+
+}
