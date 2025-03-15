bd90b1e348e566a48e10e0f9a3491f8b8415e00d
xiaohaoyong@126.com <xiaohaoyong@126.com>
2025-03-15 23:35:16 +0800

    试点入口开放

--- api/controllers/ImageController.php
+++ api/controllers/ImageController.php
@@ -16,9 +16,12 @@ use Da\QrCode\QrCode;
 class ImageController extends \yii\web\Controller
 {
     public function actionQrCode($id){
+
         //QrCode::png('appoint:'.$id,false,Enum::QR_ECLEVEL_H,10);
         $qrCode = (new QrCode('appoint:'.$id))
             ->setSize(10);
+        header('Content-Type: '.$qrCode->getContentType());
+
         echo $qrCode->writeDataUri();
     }
 
